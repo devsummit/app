ae6a2c2a4d0de059d2a902bff206c767f6feedb5
kristoforusrp <kristoforusrp@gmail.com>
2017-08-07 21:24:14 +0700

    format code style

--- app/containers/Login/index.js
+++ app/containers/Login/index.js
@@ -40,73 +40,73 @@ export default class Login extends Component {
     };
   }
 
-    onLogin = (usernameInput, passwordInput) => {
-      fetch('http://private-f30431-devsummit.apiary-mock.com/auth/login', {
-        method: 'POST',
-        headers: {
-          Accept: 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          username: usernameInput,
-          password: passwordInput
-        })
-      }).then(response => response.json())
-        .then(async (responseJson) => {
-          if (responseJson.message === 'user logged in succesfully') {
-            try {
-              await AsyncStorage.setItem('access_token', responseJson.result.access_token);
-              await AsyncStorage.setItem('refresh_token', responseJson.result.refresh_token);
-            } catch (error) {
-              Alert.alert(
-                'Failed',
-                'Failed to store token',
-              );
-            }
-
-            this.setState({ loggedIn: true });
+  onLogin = (usernameInput, passwordInput) => {
+    fetch('http://private-f30431-devsummit.apiary-mock.com/auth/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        username: usernameInput,
+        password: passwordInput
+      })
+    }).then(response => response.json())
+      .then(async (responseJson) => {
+        if (responseJson.message === 'user logged in succesfully') {
+          try {
+            await AsyncStorage.setItem('access_token', responseJson.result.access_token);
+            await AsyncStorage.setItem('refresh_token', responseJson.result.refresh_token);
+          } catch (error) {
             Alert.alert(
-              'Success',
-              'Login Success',
+              'Failed',
+              'Failed to store token',
             );
           }
-        })
-        .catch(() => {
+
+          this.setState({ loggedIn: true });
           Alert.alert(
-            'Login Failed',
-            'Something is wrong, try again later.',
+            'Success',
+            'Login Success',
           );
-        });
+        }
+      })
+      .catch(() => {
+        Alert.alert(
+          'Login Failed',
+          'Something is wrong, try again later.',
+        );
+      });
+  }
+
+  render() {
+    if (this.state.loggedIn) {
+      // Actions.register()
     }
 
-    render() {
-      if (this.state.loggedIn) {
-        // Actions.register()
-      }
-
-      return (
-        <Container style={styles.container}>
-          <Content>
-            <Form>
-              <Item floatingLabel>
-                <Label>Username</Label>
-                <Input onChangeText={username => this.setState({ username })} />
-              </Item>
-              <Item floatingLabel last>
-                <Label>Password</Label>
-                <Input secureTextEntry onChangeText={password => this.setState({ password })} />
-              </Item>
-            </Form>
-            <Button
-              primary
-              block
-              style={styles.button}
-              onPress={() => this.onLogin(this.state.username, this.state.password)}
-            >
-              <Text style={styles.buttomText}>Log In</Text>
-            </Button>
-          </Content>
-        </Container>
-      );
-    }
+    return (
+      <Container style={styles.container}>
+        <Content>
+          <Form>
+            <Item floatingLabel>
+              <Label>Username</Label>
+              <Input onChangeText={username => this.setState({ username })} />
+            </Item>
+            <Item floatingLabel last>
+              <Label>Password</Label>
+              <Input secureTextEntry onChangeText={password => this.setState({ password })} />
+            </Item>
+          </Form>
+          <Button
+            primary
+            block
+            style={styles.button}
+            onPress={() => this.onLogin(this.state.username, this.state.password)}
+          >
+            <Text style={styles.buttomText}>Log In</Text>
+          </Button>
+        </Content>
+      </Container>
+    );
+  }
 }
