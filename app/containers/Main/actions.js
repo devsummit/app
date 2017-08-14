7049e05bf1466ca74ca6967f7832d7e821232fa1
hanalaydrus <hanalaydrus@gmail.com>
2017-08-14 20:04:47 +0700

    #63 finish login with google

--- app/containers/Main/actions.js
+++ app/containers/Main/actions.js
@@ -1,7 +1,6 @@
 import { AsyncStorage } from 'react-native';
 import OAuthManager from 'react-native-oauth';
-import axios from 'axios';
-import qs from 'qs'
+
 import {
   DevSummitAxios
 } from '../../helpers';
@@ -12,7 +11,10 @@ import {
 
 import {
   UPDATE_SINGLE_FIELD,
-  UPDATE_IS_LOGGED_IN
+  UPDATE_IS_LOGGED_IN,
+  GOOGLE_CALLBACK_URL,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET
 } from './constants';
 
 
@@ -63,18 +65,15 @@ export function loginGoogle() {
     const manager = new OAuthManager('devsummit')
     manager.configure({
       google: {
-        callback_url: 'http://localhost/google',
-        client_id: '1091376735288-sgpfaq0suha3qakagrsig7bee58enkqr.apps.googleusercontent.com',
-        client_secret: 'ZdbNXvmMTy9dcAK8oW-3QPOj'
+        callback_url: GOOGLE_CALLBACK_URL,
+        client_id: GOOGLE_CLIENT_ID,
+        client_secret: GOOGLE_CLIENT_SECRET
       }
     });
     manager.authorize('google', {scopes: 'email'})
     .then(resp => {
-      console.log('provider:',resp.provider)
-      console.log('token',resp.response.credentials.idToken)
-
       if (resp.authorized) {
-        axios.post('http://192.168.8.106:5000/auth/login',{
+        DevSummitAxios.post('/auth/login',{
           provider: resp.provider,
           token: resp.response.credentials.idToken
         },{
@@ -82,11 +81,10 @@ export function loginGoogle() {
             'Content-Type': 'application/json'
           }
         }).then((response) => {
-          console.log('response',response)
-          if (response && response.data && response.meta.success) {
+          if (response && response.data && response.data.meta.success) {
             try {
-              AsyncStorage.setItem('access_token', response.data.access_token);
-              AsyncStorage.setItem('refresh_token', response.data.refresh_token);
+              AsyncStorage.setItem('access_token', response.data.data.access_token);
+              AsyncStorage.setItem('refresh_token', response.data.data.refresh_token);
             } catch (error) {
               console.log(error, 'error caught');
             }
@@ -100,25 +98,3 @@ export function loginGoogle() {
     }).catch(err => console.log(err));
   }
 }
-
-// export function loginTwitter() {
-//   return (dispatch) => {
-//     const manager = new OAuthManager('devsummit')
-//     manager.configure({
-//       twitter: {
-//         consumer_key: 'vqQkdrfuEC6sjjExgyIF1upvP',
-//         consumer_secret: 'KpGq4UhEXyjzbalQsxor87s7o5tMGRQyJLEQknKxOjDyNpYARY'
-//       }
-//     });
-//     manager.authorize('twitter')
-//     .then(resp => {
-//       console.log(resp)
-//       if (resp.authorized) {
-//         dispatch({
-//           type: UPDATE_IS_LOGGED_IN,
-//           status: true
-//         });
-//       }
-//     }).catch(err => console.log(err));
-//   }
-// }
--- app/helpers.js
+++ app/helpers.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
-import qs from 'qs';
 
 import { API_BASE_URL } from './constants';
 
-axios.defaults.headers.post['Content-Type'] = 'application/json';
-
 export const DevSummitAxios = axios.create({
-  baseURL: API_BASE_URL,
-  paramsSerializer: params => qs.stringify(params)
+  baseURL: API_BASE_URL
 });
