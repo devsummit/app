bf8e2bf222ea76110d0e9009e5b8b1e5e93bcdbf
Hana Alaydrus <hanalaydrus@gmail.com>
2017-08-16 13:55:55 +0700

    #82 prevent ListItem to render multiple times

--- app/components/ListItem/index.js
+++ app/components/ListItem/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { ListItem as ListItemNativeBase } from 'native-base'
 
-export default class Button extends Component {
+export default class ListItem extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -11,14 +11,14 @@ export default class Button extends Component {
   onClickButton = (inputFunction) => {
     this.setState({ onClick: true })
     inputFunction()
-    setTimeout(() => { this.setState({onClick: false}) }, 2000);
+    setTimeout(() => { this.setState({onClick: false}); }, 2000);
   }
 
   render() {
     return (
       <ListItemNativeBase
         {...this.props}
-        onPress={() => { !this.state.onClick ? this.onClickButton(this.props.onPress) : null }}
+        onPress = {!this.state.onClick ? () => { this.onClickButton(this.props.onPress)} : null}
       >
         {this.props.children}
       </ListItemNativeBase>
