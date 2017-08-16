0f24b438ee70d1f4b60e4cf5800181c047988510
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-08-16 14:26:20 +0700

    #68 add twitter contants

--- app/containers/RegisterMenu/constants.js
+++ app/containers/RegisterMenu/constants.js
@@ -6,4 +6,8 @@ export const GOOGLE_CLIENT_SECRET = config.GOOGLE_CLIENT_SECRET;
 
 export const FB_CLIENT_ID = config.FB_CLIENT_ID;
 export const FB_CLIENT_SECRET = config.FB_CLIENT_SECRET;
+export const TWITTER_CONSUMER_KEY = config.TWITTER_CONSUMER_KEY;
+export const TWITTER_CONSUMER_SECRET = config.TWITTER_CONSUMER_SECRET;
+export const TWITTER_CALLBACK_URL = config.TWITTER_CALLBACK_URL;
 export const UPDATE_IS_REGISTERED = 'app/containers/RegisterMenu/UPDATE_IS_REGISTERED';
+export const IS_REGISTERING = 'app/containers/RegisterMenu/IS_REGISTERING';
