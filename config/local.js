1dc3eae288ebe64ca52ea31b06d58403a48a9431
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-08-16 14:27:37 +0700

    #68 add twitter config

--- config/local.js
+++ config/local.js
@@ -1,5 +1,5 @@
 export default {
-  API_BASE_URL: 'http://10.0.3.2:5000/',
+  API_BASE_URL: 'http://192.168.40.71:5000',
   FB_CLIENT_ID: '216608565531165',
   FB_CLIENT_SECRET: '0463ed52bd8a400dd48d8e9cc246acc4',
 
@@ -12,5 +12,6 @@ export default {
   GOOGLE_CLIENT_ID: '1091376735288-sgpfaq0suha3qakagrsig7bee58enkqr.apps.googleusercontent.com',
   GOOGLE_CLIENT_SECRET: 'ZdbNXvmMTy9dcAK8oW-3QPOj',
   TWITTER_CONSUMER_KEY: 'iJoptl48l8j5OseOI1lrS3r9N',
-  TWITTER_CONSUMER_SECRET: 'eJBRnVvE0YplptEelYJOuHYw2YLdOf9v39YNnfdM6Rkv3kNShC'
-}
+  TWITTER_CONSUMER_SECRET: 'eJBRnVvE0YplptEelYJOuHYw2YLdOf9v39YNnfdM6Rkv3kNShC',
+  TWITTER_CALLBACK_URL: 'devsummit://authorize'
+};
