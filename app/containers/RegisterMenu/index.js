837ca81017760d0d3a49a7e63d46dad6d051a140
Rizal Sidik Permana <rizalsidikp24@gmail.com>
2017-08-16 14:27:12 +0700

    #68 add twitter register functionality

--- app/containers/RegisterMenu/index.js
+++ app/containers/RegisterMenu/index.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
+import { func } from 'prop-types';
 import {
   Container,
   Content,
   Text
 } from 'native-base';
-import { twitter } from 'react-native-simple-auth';
-import { Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { Actions } from 'react-native-router-flux';
 
@@ -20,10 +19,13 @@ import * as actions from './actions';
 import * as selectors from './selectors';
 
 
-
 class RegisterMenu extends Component {
   registerFacebook = () => {
     this.props.registerFacebook();
+  }
+
+  registerTwitter = () => {
+    this.props.registerTwitter();
   }
 
   render() {
@@ -35,19 +37,19 @@ class RegisterMenu extends Component {
             <Icon name="facebook" color="white" style={styles.icon} />
             <Text style={styles.buttonText} >Facebook</Text>
           </Button>
-          <Button info block style={styles.button} >
+          <Button info block style={styles.button} onPress={this.registerTwitter} >
             <Icon name="twitter" color="white" style={styles.icon} />
             <Text style={styles.buttonText} >Twitter</Text>
           </Button>
-          <Button danger block style={styles.button} onPress={() => {this.props.registerGoogle()}}>
+          <Button danger block style={styles.button} onPress={() => { this.props.registerGoogle(); }}>
             <Icon name="google-plus" color="white" style={styles.icon} />
             <Text style={styles.buttonText} >Google</Text>
           </Button>
-          <Button warning block style={styles.button} onPress={() => {Actions.registerEmail()}}>
+          <Button warning block style={styles.button} onPress={() => { Actions.registerEmail(); }}>
             <Icon name="envelope" color="white" style={styles.icon} />
             <Text style={styles.buttonText}>Email</Text>
           </Button>
-          <Button success block style={styles.button} onPress={() => {Actions.registerPhone()}}>
+          <Button success block style={styles.button} onPress={() => { Actions.registerPhone(); }}>
             <Icon name="phone" color="white" style={styles.icon} />
             <Text style={styles.buttonText}>Phone</Text>
           </Button>
@@ -56,6 +58,10 @@ class RegisterMenu extends Component {
     );
   }
 }
+
+RegisterMenu.propTypes = {
+  registerFacebook: func
+};
 
 const mapStateToProps = createStructuredSelector({
   isRegistered: selectors.isRegistered()
