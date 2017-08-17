25192c4f7903df05eea5cb94c52cb56c86d510b3
caudaganesh <caudaganesh@gmail.com>
2017-08-17 19:13:15 +0700

    add ticket list selector

---
+++ app/containers/TicketList/selectors.js
@@ -0,0 +1,15 @@
+import { createSelector } from 'reselect';
+
+
+const selectListTicketReducer = () => state => state.get('ticketList');
+
+
+export const getListTicket = () => createSelector(
+  selectListTicketReducer(),
+  state => state.get('userTicket')
+);
+
+export const getIsFetchingTicket = () => createSelector(
+  selectListTicketReducer(),
+  state => state.get('isFetchingUserTicket')
+);
