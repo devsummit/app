e106b88847b8fd3e745a731db0de451149f9b5c9
caudaganesh <caudaganesh@gmail.com>
2017-08-17 19:12:35 +0700

    add ticket list constants

---
+++ app/containers/TicketList/constants.js
@@ -0,0 +1,4 @@
+const config = require('../../../config/local').default;
+
+export const FETCH_USER_TICKET = 'app/containers/TicketList/FETCH_USER_TICKET';
+export const IS_FETCHING_USER_TICKET = 'app/containers/TicketList/IS_FETCHING_USER_TICKET'
