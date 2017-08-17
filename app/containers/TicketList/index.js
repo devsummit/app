b42bac50ce692fc1b7cadef4b39e778b06a5fb52
caudaganesh <caudaganesh@gmail.com>
2017-08-17 19:06:12 +0700

    add user list ticket from api

--- app/containers/TicketList/index.js
+++ app/containers/TicketList/index.js
@@ -5,30 +5,54 @@ import {
   List,
   ListItem,
   Button,
-  Text
+  Text,
+  Spinner
 } from 'native-base';
+import PropTypes from 'prop-types';
+import { RefreshControl } from 'react-native';
+import { createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Header from '../../components/Header';
 import styles from './styles';
+import { primaryColor } from '../../constants';
+import * as actions from './actions';
+import * as selectors from './selectors';
+
 
 class TicketList extends Component {
-  printDummy = () => {
-    let rows = [];
-    for (let i = 0; i < 20; i++) {
-      rows.push(
-        <ListItem key={i} style={styles.item}>
-          <Text style={styles.text}>Gold</Text>
-          <Button small style={styles.button}>
-            <Text style={styles.buttonText}>Transfer</Text>
-          </Button>
-        </ListItem>
-      );
+  state = {
+    isLoading: true,
+    refreshing: false
+  }
+
+  componentWillMount() {
+    this.props.fetchUserTicket();
+  }
+
+  componentDidMount() {
+
+  }
+
+  componentWillReceiveProps(prevState) {
+    if (prevState.listTicket !== this.props.listTicket) {
+      this.setState({
+        isLoading: false
+      });
     }
-    return rows;
   }
 
   render() {
+    if (this.state.isLoading) {
+      return (
+        <Container>
+          <Content>
+            <Spinner color={primaryColor} />
+          </Content>
+        </Container>
+      );
+    }
     return (
       <Container>
         <Header title="Ticket List">
@@ -36,20 +60,50 @@ class TicketList extends Component {
             <Text>Order</Text>
           </Button>
         </Header>
-        <Content>
-          <List>
-            <ListItem style={styles.item}>
-              <Text style={styles.text}>Gold</Text>
-              <Button small style={styles.button}>
-                <Text style={styles.buttonText}>Transfer</Text>
-              </Button>
-            </ListItem>
-            { this.printDummy() }
-          </List>
+        <Content refreshControl={
+          <RefreshControl
+            refreshing={this.props.isGettingUserTicket}
+            onRefresh={() => { this.props.fetchUserTicket(); }}
+          />
+        }
+        >
+          <List
+            dataArray={this.props.listTicket}
+            renderRow={(item) => {
+              return (<ListItem>
+                <Text style={styles.text}>Ticket No. {item.id}</Text>
+                <Button
+                  small
+                  style={styles.button}
+                  onPress={() => {
+                    Actions.attendeesList({ ticketId: item.id });
+                  }}
+                >
+                  <Text style={styles.buttonText}>Transfer</Text>
+                  <Icon
+                    name="exchange"
+                    color="white"
+                  />
+                </Button>
+              </ListItem>);
+            }
+            }
+          />
         </Content>
       </Container>
     );
   }
 }
 
-export default TicketList;
+TicketList.propTypes = {
+  listTicket: PropTypes.array.isRequired,
+  isGettingUserTicket: PropTypes.bool.isRequired,
+  fetchUserTicket: PropTypes.func.isRequired
+};
+
+const mapStateToProps = createStructuredSelector({
+  listTicket: selectors.getListTicket(),
+  isGettingUserTicket: selectors.getIsFetchingTicket()
+});
+
+export default connect(mapStateToProps, actions)(TicketList);
