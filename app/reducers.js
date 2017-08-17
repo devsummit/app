c6693cf488ad977f28a798ded897faf876433413
caudaganesh <caudaganesh@gmail.com>
2017-08-17 19:10:20 +0700

    add ticket list and attendee list reducer

--- app/reducers.js
+++ app/reducers.js
@@ -6,13 +6,17 @@ import RegisterMenuReducer from './containers/RegisterMenu/reducer';
 import RegisterPhoneReducer from './containers/RegisterPhone/reducer';
 import RegisterEmailReducer from './containers/RegisterEmail/reducer';
 import ChangePasswordReducer from './containers/ChangePassword/reducer';
+import TicketListReducer from './containers/TicketList/reducer';
+import AttendeesListReducer from './containers/AttendeesList/reducer';
 
 const rootReducers = combineReducers({
   main: MainReducer,
   registerMenu: RegisterMenuReducer,
   registerEmail: RegisterEmailReducer,
   registerPhone: RegisterPhoneReducer,
-  changePassword: ChangePasswordReducer
+  changePassword: ChangePasswordReducer,
+  ticketList: TicketListReducer,
+  attendeesList: AttendeesListReducer
 })
 
 export default rootReducers;
