c03ee76ec0d2b72b3611b0b1d0f47a990c0df0e6
Hana Alaydrus <hanalaydrus@gmail.com>
2017-08-22 16:11:53 +0700

    #97 fix config/local

--- config/local.js
+++
@@ -1,8 +0,0 @@
-export default {
-  API_BASE_URL: 'http://192.168.40.81:5000/',
-  FB_CLIENT_ID: '216608565531165',
-  FB_CLIENT_SECRET: '0463ed52bd8a400dd48d8e9cc246acc4',
-  GOOGLE_CALLBACK_URL: 'http://localhost/google',
-  GOOGLE_CLIENT_ID: '1091376735288-sgpfaq0suha3qakagrsig7bee58enkqr.apps.googleusercontent.com',
-  GOOGLE_CLIENT_SECRET: 'ZdbNXvmMTy9dcAK8oW-3QPOj'
-}
