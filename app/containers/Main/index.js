5f18c8d95dcb5329287d096255d790157b11cfbb
akbar <akbar_rachman@ymail.com>
2017-08-28 13:16:47 +0700

    enable scroll for smaller device

--- app/containers/Main/index.js
+++ app/containers/Main/index.js
@@ -116,7 +116,7 @@ class Main extends Component {
       <Container style={styles.container}>
         <StatusBar hidden />
         <Header androidStatusBarColor="#f39e21" style={{display:'none'}}/>
-        <Content scrollEnabled={false}>
+        <Content>
           <ModalComponent
             visible={this.state.modalVisible}
             modalTitle="Subscriber"
