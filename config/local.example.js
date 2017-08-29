89033bad5b38c539ec4144e8c11d7d82761ff30e
kristoforusrp <kristoforusrp@gmail.com>
2017-08-29 12:05:56 +0700

    fix linter

--- config/local.example.js
+++ config/local.example.js
@@ -12,4 +12,4 @@ export default {
   GOOGLE_CALLBACK_URL: 'http://localhost/google',
   GOOGLE_CLIENT_ID: '1091376735288-sgpfaq0suha3qakagrsig7bee58enkqr.apps.googleusercontent.com',
   GOOGLE_CLIENT_SECRET: 'ZdbNXvmMTy9dcAK8oW-3QPOj'
-}
+};
