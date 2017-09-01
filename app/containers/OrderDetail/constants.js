3a732a3389bbae423f2c2f264e1b2325674c93de
caudaganesh <caudaganesh@gmail.com>
2017-09-01 17:13:42 +0700

    add order update status and reset state

--- app/containers/OrderDetail/constants.js
+++ app/containers/OrderDetail/constants.js
@@ -1,4 +1,6 @@
 export const SET_TICKET_TYPE = 'app/containers/OrderDetail/SET_TICKET_TYPE';
 export const UPDATE_ORDER = 'app/containers/OrderDetail/UPDATE_ORDER';
 export const SET_ORDER = 'app/containers/OrderDetail/SET_ORDER';
-export const IS_UPDATING_ORDER = 'app/containes/OrderDetail/IS_UPDATING_ORDER';
+export const IS_UPDATING_ORDER = 'app/containers/OrderDetail/IS_UPDATING_ORDER';
+export const UPDATE_ORDER_STATUS = 'app/containers/OrderDetail/UPDATE_ORDER_STATUS';
+export const RESET_STATE = 'app/containers/OrderDetail/UPDATE_ORDER_DETAIL';
