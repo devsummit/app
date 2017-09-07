fd22565054ff5440d61abe9f2b227c83104899ca
rendisaepul <rendisaepulbahri09@gmail.com>
2017-09-07 19:13:45 +0700

    create selector

---
+++ app/containers/Schedule/selectors.js
@@ -0,0 +1,8 @@
+import { createSelector } from 'reselect';
+
+const selectListScheduleReducer = () => state => state.get('scheduleList');
+
+export const getListSchedule = () => createSelector(
+    selectListScheduleReducer(),
+    state => state.get('userSchedule')
+);
