6b4731991ad8bfd0425c4d67f9da3ab170ade94f
rendisaepul <rendisaepulbahri09@gmail.com>
2017-09-08 17:07:04 +0700

    add user name speaker

--- app/components/ScheduleList/index.js
+++ app/components/ScheduleList/index.js
@@ -20,6 +20,8 @@ class ScheduleList extends Component{
                         title={event.event.title}
                         description={event.event.information}
                         stage={event.stage.name}
+                        first_name={event.user.first_name}
+                        last_name={event.user.last_name}
                         time_start={event.time_start}
                         time_end={event.time_end}
                     />
