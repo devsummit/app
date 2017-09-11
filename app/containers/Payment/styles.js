1ea2e7602ec88658c82b6228a6de4e9c14c0b7ed
Arnaz <arnazmursidp.arnazmursid@gmail.com>
2017-09-11 20:24:03 +0700

    #154 indomaret payment

--- app/containers/Payment/styles.js
+++ app/containers/Payment/styles.js
@@ -7,18 +7,39 @@ const styles = StyleSheet.create({
   text: {
     flex: 3
   },
+  pickerWrapper: {
+    borderColor: '#FFD740',
+    borderWidth: 2,
+    padding: 3,
+    margin: 10,
+    borderRadius: 50,
+    width: '70%',
+    marginBottom: 'auto',
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
+  littleText: {
+    color: '#FFD740',
+    width: '70%',
+    fontWeight: "bold",
+    fontSize: 13,
+    marginTop: 20,
+    marginLeft: 'auto',
+    marginRight: 'auto'
+  },
   button: {
     flex: 0.4,
     justifyContent: 'center',
     borderRadius: 30,
     width: '70%',
+    marginTop: 20,
     marginLeft: 'auto',
     marginRight: 'auto',
   },
   roundButton: {
     borderRadius: 90,
     backgroundColor: 'rgba(52, 52, 52, 0.7)',
-    marginRight: 10
+    marginRight: 1
   },
   textButton: {
     color: 'white',
