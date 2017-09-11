c9331a8c1146fb6a9e16c0237e4d1f6160112fa2
akbar <akbar_rachman@ymail.com>
2017-09-11 20:24:51 +0700

    clean up button component

--- app/components/Button/index.js
+++ app/components/Button/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { Button as ButtonNativeBase } from 'native-base';
-import PropTypes from 'prop-types';
 
 export default class Button extends Component {
   constructor(props) {
@@ -13,7 +12,7 @@ export default class Button extends Component {
 
   componentWillUnmount() {
     if (this.state.timeout !== null) {
-      clearTimeout(this.state.timeout)
+      clearTimeout(this.state.timeout);
     }
   }
 
@@ -21,8 +20,8 @@ export default class Button extends Component {
     this.setState({ onClick: true });
     inputFunction();
 
-    const to = setTimeout(() => { this.setState({ onClick: false }); }, 2000)
-    this.setState({timeout: to})
+    const to = setTimeout(() => { this.setState({ onClick: false }); }, 2000);
+    this.setState({ timeout: to });
   }
 
   render() {
