f4bfa449c8051967a5cc6aefa0d5f4569f1b3556
akbar <akbar_rachman@ymail.com>
2017-09-11 20:50:58 +0700

    fix indentation and use pure function

--- app/components/ScheduleList/index.js
+++ app/components/ScheduleList/index.js
@@ -1,34 +1,26 @@
-import React, { Component } from 'react';
-import { ScrollView, View, Text } from 'react-native';
+import React from 'react';
+import { ScrollView } from 'react-native';
 
 import ScheduleCard from '../ScheduleCard';
-import styles from './styles';
 
-class ScheduleList extends Component{
-
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const { events } = this.props;
-        return (
-            <ScrollView>
-                { events.length > 0 && events.map((event, index)=> (
-                    <ScheduleCard
-                        key={index}
-                        title={event.event.title}
-                        description={event.event.information}
-                        stage={event.stage.name}
-                        first_name={event.user.first_name}
-                        last_name={event.user.last_name}
-                        time_start={event.time_start}
-                        time_end={event.time_end}
-                    />
-                ))}
-            </ScrollView>
-        )
-    }
+const ScheduleList = () => {
+  const { events } = this.props;
+  return (
+    <ScrollView>
+      { events.length > 0 && events.map((event, index) => (
+        <ScheduleCard
+          key={index}
+          title={event.event.title}
+          description={event.event.information}
+          stage={event.stage.name}
+          first_name={event.user.first_name}
+          last_name={event.user.last_name}
+          time_start={event.time_start}
+          time_end={event.time_end}
+        />
+      ))}
+    </ScrollView>
+  );
 };
 
 export default ScheduleList;
