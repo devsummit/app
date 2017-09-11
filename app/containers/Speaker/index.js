7b8044479ef77ba1675eaf72cce613e1d7bdc75f
denisenricohasyim93 <denisenricohasyim93@gmail.com>
2017-09-11 19:09:24 +0700

    add search function and implements it

--- app/containers/Speaker/index.js
+++ app/containers/Speaker/index.js
@@ -2,28 +2,34 @@ import React, { Component } from 'react';
 import {
   Container,
   Content,
+  Header,
   Button,
+  Item,
+  Icon,
+  Input,
   Text,
   Card,
   CardItem,
   Body
 } from 'native-base';
 import { View, StyleSheet, Alert, Image, KeyboardAvoidingView } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import IconFA from 'react-native-vector-icons/FontAwesome';
 import { Actions } from 'react-native-router-flux';
 import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
-import Header from '../../components/Header';
+import HeaderPoint from '../../components/Header';
 import styles from './styles';
 import * as selectors from './selectors';
 import * as actions from './actions';
+
 
 class Speaker extends Component {
   constructor(props) {
     super(props);
     this.state = {
       firstName: 'John',
-      lastName: 'Doe'
+      lastName: 'Doe',
+      speakerFilter: []
     };
   }
 
@@ -31,13 +37,48 @@ class Speaker extends Component {
     this.props.fetchSpeakerList();
   }
 
+  componentWillReceiveProps(prevProps) {
+    if (prevProps && prevProps.speaker && this.props.speaker !== prevProps.speaker) {
+      this.setState({
+        speakerFilter: prevProps.speaker
+      });
+    }
+  }
+
+
+  handleInputChange = (field, value) => {
+    this.props.updateInputFields(field, value);
+    this.props.updateErrorFields(`error_${field}`, value = !(value.length > 0));
+  }
+
+  handleFilter = (param) => {
+    const filteredSpeaker = [];
+    this.props.speaker.map((data) => {
+      if (data.user.first_name.toLowerCase().includes(param.toLowerCase()) || data.user.last_name.toLowerCase().includes(param.toLowerCase())) {
+        filteredSpeaker.push(data);
+      }
+    });
+    this.setState({
+      speakerFilter: filteredSpeaker
+    });
+  }
 
   render() {
     return (
       <Container>
-        <Header title="SPEAKER" />
-        <Content style={styles.content}>
-          {this.props.speaker.map(data => (
+        <HeaderPoint title="SPEAKER" />
+        <Header searchBar style={styles.searchHeader} androidStatusBarColor="#f39e21">
+          <Item>
+            <Icon name="ios-search" />
+            <Input
+              placeholder="Search speaker ..."
+              onChangeText={text => this.handleFilter(text)}
+            />
+            <Icon name="ios-calendar" />
+          </Item>
+        </Header>
+        <Content>
+          {this.state.speakerFilter.map(data => (
             <Card key={data.id}>
               <CardItem>
                 <Body>
@@ -45,11 +86,11 @@ class Speaker extends Component {
                     <View style={styles.profileSection}>
                       <Image
                         style={styles.profilePic}
-                        source={{uri: data.profile_picture}}
+                        source={{ uri: data.profile_picture }}
                       />
                     </View>
                     <View style={styles.nameSection}>
-                      <Text style={styles.name}>{data.full_name}</Text>
+                      <Text style={styles.name}>{data.user.first_name} {data.user.last_name}</Text>
                       <Text style={styles.job}>{data.job}</Text>
                       <Text numberOfLines={3} style={styles.summary} >
                         {data.summary}
@@ -78,11 +119,12 @@ class Speaker extends Component {
           ))}
         </Content>
       </Container>
-    )
+
+    );
   }
 }
 
 const mapStateToProps = createStructuredSelector({
   speaker: selectors.getListSpeaker()
-})
+});
 export default connect(mapStateToProps, actions)(Speaker);
