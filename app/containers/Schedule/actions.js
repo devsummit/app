badb3fa4062555b8538c194a4fedddcbbdc8a0da
rendisaepul <rendisaepulbahri09@gmail.com>
2017-09-13 16:56:06 +0700

    change end point

--- app/containers/Schedule/actions.js
+++ app/containers/Schedule/actions.js
@@ -4,21 +4,22 @@ import {
 } from '../../helpers';
 
 import { FETCH_USER_SCHEDULE } from './constants';
+import { orderByDay } from './index';
 
 export function fetchUserSchedule() {
     return (dispatch) => {
         getAccessToken()
         .then((token) => {
             const headers = { Authorization: token };
-            DevSummitAxios.get('api/v1/schedules', { headers })
+            DevSummitAxios.get('api/v1/schedules' + orderByDay, { headers })
                 .then((response) => {
                     dispatch({
                         type: FETCH_USER_SCHEDULE,
                         payloads: response.data.data
                     });
+                    console.log(response.data);
                 })
                 .catch((error) => { return console.log(error.response); });
         }).catch((err) => { console.log(err); });
     };
 }
-
