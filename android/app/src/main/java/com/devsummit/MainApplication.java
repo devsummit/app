d4adad94e91f4ff16761ac38a1f69758c177ca73
rendisaepul <rendisaepulbahri09@gmail.com>
2017-09-14 23:54:53 +0700

    add libary document picker

--- android/app/src/main/java/com/devsummit/MainApplication.java
+++ android/app/src/main/java/com/devsummit/MainApplication.java
@@ -3,6 +3,7 @@ package com.devsummit;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativedocumentpicker.ReactNativeDocumentPicker;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.fullstack.oauth.OAuthManagerPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeDocumentPicker(),
           new VectorIconsPackage(),
           new PickerPackage(),
           new OAuthManagerPackage(),
