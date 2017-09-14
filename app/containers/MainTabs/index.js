468935171763fd0e89fcc423e73f529dd2f40d8d
rendisaepul <rendisaepulbahri09@gmail.com>
2017-09-14 23:48:03 +0700

    add condition for user login as spekear or else

--- app/containers/MainTabs/index.js
+++ app/containers/MainTabs/index.js
@@ -1,15 +1,25 @@
-import { View } from 'react-native';
 import React, { Component } from 'react';
+import { View, AsyncStorage } from 'react-native';
 import { Tabs, Tab, TabHeading, Container } from 'native-base';
 import IconSimpleLine from 'react-native-vector-icons/SimpleLineIcons';
+import Toast from 'react-native-simple-toast';
 import Schedule from '../Schedule';
 import Speaker from '../Speaker';
 import Ticket from '../TicketList';
 import Profile from '../Profile';
+import MaterialList from '../MaterialList';
 
 export default class MainTabs extends Component {
   state={
-    currentTab: 0
+    currentTab: 0,
+    roleId: null
+  }
+
+  componentWillMount() {
+    AsyncStorage.getItem('role_id')
+    .then(id => {
+      this.setState({ roleId: JSON.parse(id) });
+    }).catch(e => Toast.show('Error getting role id'));
   }
 
   handleCurrentTab = (number) => {
@@ -17,20 +27,45 @@ export default class MainTabs extends Component {
   }
 
   render() {
+    const speaker = this.state.roleId === 4;
     return (
       <Container>
         <View style={{ flex: 1 }}>
           <Tabs onChangeTab={(i, ref) => this.handleCurrentTab(i.i)} tabBarPosition="bottom" initialPage={0}>
-            <Tab heading={<TabHeading style={{ backgroundColor: 'white' }}><IconSimpleLine name="calendar" style={[ this.state.currentTab === 0 ? { color: '#f39e21' } : null, { fontSize: 18 } ]} /></TabHeading>}>
+            <Tab 
+              heading={
+                <TabHeading style={{ backgroundColor: 'white' }}>
+                  <IconSimpleLine
+                    name="calendar"
+                    style={[ this.state.currentTab === 0 ? { color: '#f39e21' } : null, { fontSize: 18 } ]} />
+                </TabHeading>}>
               <Schedule />
             </Tab>
-            <Tab heading={<TabHeading style={{ backgroundColor: 'white' }}><IconSimpleLine name="people" style={[ this.state.currentTab === 1 ? { color: '#f39e21' } : null, { fontSize: 18 } ]} /></TabHeading>}>
+            <Tab 
+              heading={
+                <TabHeading style={{ backgroundColor: 'white' }}>
+                  <IconSimpleLine
+                    name="people"
+                    style={[ this.state.currentTab === 1 ? { color: '#f39e21' } : null, { fontSize: 18 } ]} />
+                </TabHeading>}>
               <Speaker />
             </Tab>
-            <Tab heading={<TabHeading style={{ backgroundColor: 'white' }}><IconSimpleLine name="wallet" style={[ this.state.currentTab === 2 ? { color: '#f39e21' } : null, { fontSize: 18 } ]} /></TabHeading>}>
-              <Ticket />
+            <Tab
+              heading={
+                <TabHeading style={{ backgroundColor: 'white' }}>
+                  <IconSimpleLine
+                    name="wallet"
+                    style={[ this.state.currentTab === 2 ? { color: '#f39e21' } : null, { fontSize: 18 } ]} />
+                </TabHeading>}>
+            { speaker ? <MaterialList /> : <Ticket /> }
             </Tab>
-            <Tab heading={<TabHeading style={{ backgroundColor: 'white' }}><IconSimpleLine name="user" style={[ this.state.currentTab === 3 ? { color: '#f39e21' } : null, { fontSize: 18 } ]} /></TabHeading>}>
+            <Tab
+              heading={
+                <TabHeading style={{ backgroundColor: 'white' }}>
+                  <IconSimpleLine
+                    name="user"
+                    style={[ this.state.currentTab === 3 ? { color: '#f39e21' } : null, { fontSize: 18 } ]} />
+                </TabHeading>}>
               <Profile />
             </Tab>
           </Tabs>
