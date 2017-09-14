d383f594053cb37d66417e06ac568adaacbd98d5
akbar <akbar_rachman@ymail.com>
2017-09-15 03:37:04 +0700

    fix state on reducers

--- app/containers/Profile/reducer.js
+++ app/containers/Profile/reducer.js
@@ -22,6 +22,8 @@ const initialState = fromJS({
     firstName: '',
     lastName: '',
     boothInfo: '',
+    job: '',
+    summary: '',
     profilePic: 'https://museum.wales/media/40374/thumb_480/empty-profile-grey.jpg'
   },
   avatar: 'https://museum.wales/media/40374/thumb_480/empty-profile-grey.jpg',
