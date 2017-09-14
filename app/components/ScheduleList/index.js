7b37b05d986684f5062fb3162b0c5599d134ae40
akbar <akbar_rachman@ymail.com>
2017-09-14 21:07:52 +0700

    add constructor

--- app/components/ScheduleList/index.js
+++ app/components/ScheduleList/index.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { ScrollView } from 'react-native';
 
 import ScheduleCard from '../ScheduleCard';
 
-const ScheduleList = () => {
-  const { events } = this.props;
-  return (
-    <ScrollView>
-      { events.length > 0 && events.map((event, index) => (
-        <ScheduleCard
-          key={index}
-          title={event.event.title}
-          description={event.event.information}
-          stage={event.stage.name}
-          first_name={event.user.first_name}
-          last_name={event.user.last_name}
-          time_start={event.time_start}
-          time_end={event.time_end}
-        />
-      ))}
-    </ScrollView>
-  );
-};
+export default class ScheduleList extends Component {
+  // Use constructor to get event props
+  constructor (props) {
+    super(props);
+  }
 
-export default ScheduleList;
+  render() {
+    const { events } = this.props;
+    return (
+      <ScrollView>
+        { events.length > 0 && events.map((event, index) => (
+          <ScheduleCard
+            key={index}
+            title={event.event.title}
+            description={event.event.information}
+            stage={event.stage.name}
+            first_name={event.user.first_name}
+            last_name={event.user.last_name}
+            time_start={event.time_start}
+            time_end={event.time_end}
+          />
+        ))}
+      </ScrollView>
+    );
+  }
+}
