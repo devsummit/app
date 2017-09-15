025c60d5337274f5ad3426e2bcd2f36ff9507aab
rendisaepul <rendisaepulbahri09@gmail.com>
2017-09-15 20:29:08 +0700

    add status

--- app/containers/MaterialList/reducer.js
+++ app/containers/MaterialList/reducer.js
@@ -2,11 +2,13 @@ import { fromJS } from 'immutable';
 
 import {
   FETCH_MATERIAL_LIST,
-  UPDATE_SINGLE_INPUT_FIELD
+  UPDATE_SINGLE_INPUT_FIELD,
+  UPDATE_MODAL_STATUS
 } from './constants';
 
 const initialState = fromJS({
   material: [],
+  visible: false,
   fields: {
     title: '',
     summary: '',
@@ -18,6 +20,8 @@ function materialListReducer(state = initialState, action) {
   switch (action.type) {
     case FETCH_MATERIAL_LIST:
       return state.set('material', action.payloads);
+    case UPDATE_MODAL_STATUS:
+      return state.set('visible', action.status);
     case UPDATE_SINGLE_INPUT_FIELD:
       return state.setIn([ 'fields', action.field ], action.value);
     default:
