2f5484c18b44c656d54cac91b0429b1229eb45f6
rendisaepul <rendisaepulbahri09@gmail.com>
2017-09-15 20:20:43 +0700

    add const for update

--- app/containers/MaterialList/constants.js
+++ app/containers/MaterialList/constants.js
@@ -1,2 +1,3 @@
 export const FETCH_MATERIAL_LIST = 'app/containers/Speaker/FETCH_MATERIAL_LIST';
 export const UPDATE_SINGLE_INPUT_FIELD = 'app/containers/MaterialList/UPDATE_SINGLE_INPUT_FIELD';
+export const UPDATE_MODAL_STATUS = 'app/containers/MaterialList/UPDATE_MODAL_STATUS';
