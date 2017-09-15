ca539f1a30c31d8013b145af8382ea5492c57681
rendisaepul <rendisaepulbahri09@gmail.com>
2017-09-15 20:30:55 +0700

    clean up

--- app/containers/MaterialList/styles.js
+++ app/containers/MaterialList/styles.js
@@ -9,10 +9,6 @@ const styles = StyleSheet.create({
     paddingLeft: 5,
     paddingRight: 5
   },
-  searchHeader: {
-    marginTop: 0,
-    backgroundColor: 'rgb(255,255,255)'
-  },
   bodySection: {
     flex: 1,
     flexDirection: 'row',
@@ -25,20 +21,10 @@ const styles = StyleSheet.create({
     marginLeft: 15,
     flex: 8
   },
-  profilePic: {
-    height: 60,
-    width: 60,
-    borderRadius: 50
-  },
   name: {
     color: '#333',
     fontSize: 18,
     fontWeight: 'bold'
-  },
-  job: {
-    color: '#333',
-    fontSize: 14,
-    opacity: 0.8
   },
   summary: {
     color: '#333',
@@ -48,11 +34,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'flex-end',
     justifyContent: 'flex-end'
-  },
-  seeMoreLine: {
-    height: 1,
-    opacity: 0.1,
-    backgroundColor: 'black'
   },
   footerButton: {
     borderRadius: 50,
