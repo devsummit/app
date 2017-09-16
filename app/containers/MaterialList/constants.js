82f23b64d7d80ce23b13fa64c242acaa292e0d17
caudaganesh <caudaganesh@gmail.com>
2017-09-16 22:17:06 +0700

    add isfetching

--- app/containers/MaterialList/constants.js
+++ app/containers/MaterialList/constants.js
@@ -1,3 +1,5 @@
 export const FETCH_MATERIAL_LIST = 'app/containers/Speaker/FETCH_MATERIAL_LIST';
 export const UPDATE_SINGLE_INPUT_FIELD = 'app/containers/MaterialList/UPDATE_SINGLE_INPUT_FIELD';
 export const UPDATE_MODAL_STATUS = 'app/containers/MaterialList/UPDATE_MODAL_STATUS';
+export const ADD_MATERIAL_ITEM = 'app/containers/MaterialList/ADD_MATERIAL_ITEM';
+export const IS_FETCHING_MATERIAL = 'app/containers/MaterialList/IS_FETCHING_MATERIAL';
