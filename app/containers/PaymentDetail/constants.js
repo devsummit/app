d60ebd50a370ee057325e2db77b5737c80d9129e
caudaganesh <caudaganesh@gmail.com>
2017-09-16 18:46:16 +0700

    #199 add reset response

--- app/containers/PaymentDetail/constants.js
+++ app/containers/PaymentDetail/constants.js
@@ -2,3 +2,4 @@ export const UPDATE_SINGLE_INPUT_FIELD = 'app/containers/PaymentDetail/UPDATE_SI
 export const UPDATE_SINGLE_ERROR_FIELD = 'app/containers/PaymentDetail/UPDATE_SINGLE_ERROR_FIELD';
 export const GET_TRANSACTION_RESPONSE = 'app/containers/PaymentDetail/GET_TRANSACTION_REPONSE';
 export const IS_FETCHING_TRANSACTION = 'app/containers/PaymentDetail/IS_FETCHING_TRANSACTION';
+export const CLEAR_TRANSACTION_RESPONSE = 'app/containers/PaymentDetail/CLEAR_TRANSACTION_RESPONSE';
