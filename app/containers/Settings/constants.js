095c4d650b899837a7f2f49059952238e03255e8
denisenricohasyim93 <denisenricohasyim93@gmail.com>
2017-09-18 20:07:22 +0700

    add setting component

--- app/containers/Settings/constants.js
+++
@@ -1,6 +0,0 @@
-export const UPDATE_SINGLE_FIELD = 'app/containers/Profile/UPDATE_FIELD';
-export const UPDATE_IS_PROFILE_UPDATED = 'app/containers/Profile/UPDATE_IS_PROFILE_UPDATED';
-export const UPDATE_AVATAR = 'app/containers/Profile/UPDATE_AVATAR';
-export const UPDATE_IS_AVATAR_UPDATED = 'app/containers/Profile/UPDATE_IS_AVATAR_UPDATED';
-export const UPDATE_IS_LOG_OUT = 'app/containers/Profile/UPDATE_IS_LOG_OUT';
-export const UPDATE_IS_DISABLED = 'app/containers/Profile/UPDATE_IS_DISABLED';
--- app/containers/Settings/index.js
+++ app/containers/Settings/index.js
@@ -22,87 +22,11 @@ import Button from '../../components/Button';
 import Header from '../../components/Header';
 import styles from './styles';
 
-import * as actions from './actions';
-import * as selectors from './selectors';
-
 class Profile extends Component {
   state = {
     id: null
   }
-  componentWillMount() {
-    getProfileData().then((profileData) => {
-      if (profileData) {
-        this.handleUpdateAvatar(profileData.photos[0].url);
-        this.handleInputChange('username', profileData.username);
-        this.handleInputChange('firstName', profileData.first_name);
-        this.handleInputChange('lastName', profileData.last_name);
-        if (profileData.role_id === 3) {
-          this.handleInputChange('boothInfo', profileData.booth.summary);
-        }
-        if (profileData.role_id === 4) {
-          this.handleInputChange('job', profileData.speaker.job);
-          this.handleInputChange('summary', profileData.speaker.summary);
-        }
-      }
-    });
-    AsyncStorage.getItem('role_id')
-      .then((roleId) => {
-        const id = JSON.parse(roleId);
-        this.setState({ id });
-      }).catch(() => console.log('Error'));
-  }
-
-  componentWillReceiveProps(prevProps) {
-    if (prevProps.isProfileUpdated !== this.props.isProfileUpdated) {
-      Alert.alert('Success', 'Profile has been changed');
-      this.props.updateIsProfileUpdated(false);
-    }
-
-    if (prevProps.isAvatarUpdated !== this.props.isAvatarUpdated) {
-      Alert.alert('Success', 'Avatar has been changed');
-      this.props.updateIsAvatarUpdated(false);
-    }
-
-    if (prevProps.isLogOut !== this.props.isLogOut) {
-      Actions.main();
-      this.props.updateIsLogOut(false);
-    }
-  }
-
-  handleInputChange = (field, value) => {
-    this.props.updateFields(field, value);
-  }
-
-  handleUpdateAvatar = (value) => {
-    this.props.updateAvatar(value);
-  }
-
-  uploadImage = () => {
-    ImagePicker.openPicker({
-      width: 300,
-      height: 400,
-      cropping: true,
-      includeBase64: true
-    }).then((image) => {
-      this.props.updateImage(image);
-    }).catch(err => console.log('Error getting image from library', err));
-  }
-
   render() {
-    // destructure state
-    const booth = this.state.id === 3;
-    const speaker = this.state.id === 4;
-    const { fields, isDisabled, avatar, errorFields } = this.props || {};
-    const {
-      firstName,
-      lastName,
-      username,
-      boothInfo,
-      job,
-      summary,
-      profilePic
-    } = fields || '';
-
     return (
     <Container>
       <Content>
@@ -144,16 +68,4 @@ class Profile extends Component {
   }
 }
 
-/**
- *  Map redux state to component props
- */
-const mapStateToProps = createStructuredSelector({
-  fields: selectors.getFields(),
-  isProfileUpdated: selectors.getIsProfileUpdated(),
-  avatar: selectors.getAvatar(),
-  isAvatarUpdated: selectors.getIsAvatarUpdated(),
-  isDisabled: selectors.getIsDisabled(),
-  isLogOut: selectors.getIsLogOut()
-});
-
-export default connect(mapStateToProps, actions)(Profile);
+export default connect(null, null)(Profile);
