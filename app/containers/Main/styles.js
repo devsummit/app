0be61d03bf082de12813fc7c15e6cddc20a6b89d
akbar <akbar_rachman@ymail.com>
2017-09-19 00:37:07 +0700

    clean up style

--- app/containers/Main/styles.js
+++ app/containers/Main/styles.js
@@ -9,12 +9,6 @@ const styles = StyleSheet.create({
     width: undefined,
     height: undefined
   },
-  titleText: {
-    fontSize: 10.5,
-    color: 'white',
-    fontFamily: 'Montserrat',
-    fontWeight: 'bold'
-  },
   formSection: {
     marginTop: 8,
     marginRight: 18,
@@ -30,25 +24,6 @@ const styles = StyleSheet.create({
     flex: 1,
     fontSize: 18,
     alignSelf: 'center'
-  },
-  buttonSocialSection: {
-    flex: 1,
-    flexDirection: 'row',
-    marginLeft: 12,
-    marginRight: 12,
-    marginTop: 5,
-    marginBottom: 5
-  },
-  buttonSocial: {
-    justifyContent: 'center',
-    marginLeft: 5,
-    borderColor: 'transparent',
-    borderWidth: 1,
-    borderRadius: 30,
-    marginRight: 5,
-    flexDirection: 'row',
-    elevation: 0,
-    flex: 0.3
   },
   button: {
     width: '90%',
@@ -88,21 +63,7 @@ const styles = StyleSheet.create({
     position: 'relative',
     flex: 1,
     flexDirection: 'row',
-    marginRight: 18,
-    marginLeft: 18,
-    marginBottom: 5
-  },
-  lineTextOne: {
-    flex: 4.5,
-    backgroundColor: '#FFFFFF',
-    height: 1.5,
-    marginTop: 12
-  },
-  lineTextTwo: {
-    textAlign: 'center',
-    flex: 1,
-    fontSize: 16,
-    color: '#FFFFFF'
+    margin: 12
   },
   lineTextThree: {
     flex: 2,
@@ -115,13 +76,6 @@ const styles = StyleSheet.create({
     flex: 6,
     color: '#FFFFFF'
   },
-  loginMethod: {
-    textAlign: 'center',
-    fontSize: 16,
-    textDecorationLine: 'underline'
-  },
-  registerSection: {
-  },
   registerText: {
     textAlign: 'center',
     color: '#FFFFFF'
@@ -131,12 +85,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginLeft: 8,
     color: '#FFD740'
-  },
-  spinner: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    backgroundColor: '#f39e21'
   },
   item: {
     marginBottom: 10,
