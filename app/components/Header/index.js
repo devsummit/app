2255c1e74c00f9e7b9315979b0df195d373180c3
rendisaepul <rendisaepulbahri09@gmail.com>
2017-09-18 17:37:00 +0700

    delete text points and icon

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -3,7 +3,6 @@ import {
   Text
 } from 'native-base';
 import { View } from 'react-native';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import LinearGradient from 'react-native-linear-gradient';
 
 import styles from './styles';
@@ -17,10 +16,6 @@ const Header = (props) => {
         <View style={styles.header}>
           <View style={styles.headerText}>
             <Text style={styles.pageTitle}>{props.title}</Text>
-            <View style={styles.pointSection}>
-              <Icon name="coin" style={styles.coin} />
-              <Text style={styles.points}> 1000 pts</Text>
-            </View>
           </View>
           <View style={styles.content}>
             {props.children}
