8aad9d55e438bc9bd6df6b3d9743800885f3b780
kristoforusrp <kristoforusrp@gmail.com>
2017-09-21 01:37:05 +0700

    add more constants

--- app/containers/Feed/constants.js
+++ app/containers/Feed/constants.js
@@ -2,3 +2,5 @@ export const FETCH_FEEDS = 'app/containers/Feed/FETCH_FEEDS';
 export const IS_FETCHING_FEEDS = 'app/containers/Feed/IS_FETCHING_FEEDS';
 export const POST_FEEDS = 'app/containers/Feed/POST_FEEDS';
 export const IS_POST_FEEDS = 'app/containers/Feed/IS_POST_FEEDS';
+export const UPDATE_IMAGE = 'app/containers/Feed/UPDATE_IMAGE';
+export const UPDATE_TEXT = 'app/containers/Feed/UPDATE_TEXT';
