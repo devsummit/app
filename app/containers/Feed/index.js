36c58c613689ce549e626018ca4e0b97d7804647
rendisaepul <rendisaepulbahri09@gmail.com>
2017-09-20 22:17:44 +0700

    add fab for redeem

--- app/containers/Feed/index.js
+++ app/containers/Feed/index.js
@@ -6,7 +6,8 @@ import {
   Text,
   Tabs,
   Tab,
-  TabHeading
+  TabHeading,
+  Fab
 } from 'native-base';
 import PropTypes from 'prop-types';
 import { RefreshControl, View } from 'react-native';
@@ -17,15 +18,32 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './styles';
 import {PRIMARYCOLOR} from '../../constants';
 import HeaderPoint from '../../components/Header';
+import ModalRedeem from '../../components/ModalRedeem';
+import * as actions from './actions';
+import * as selectors from './selectors';
 
 import TicketList from '../TicketList';
 
 class Feed extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      active: false,
+      invisible: false
+    };
+  }
+
+  handleInputChange = (field, value) => {
+    this.props.updateInputFields(field, value);
+  }
+
+  placeRedeem = () => {
+    this.props.placeRedeem();
+    this.setState({ invisible: !this.state.invisible });
   }
 
   render() {
+    console.log('here props', this.props);
     return (
       <Container
         style={styles.container}
@@ -118,9 +136,36 @@ class Feed extends Component {
               <TicketList />
           </Tab>
         </Tabs>
+        <Fab
+          active={ this.state.active }
+          direction='up'
+          style={{ backgroundColor: '#FFA726' }}
+          position='bottomRight'
+          onPress={() => this.setState({ active: !this.state.active })}
+        >
+          <Icon name='plus' />
+          <Button
+            onPress={() => this.setState({invisible: !this.state.invisible})}
+          >
+            <Icon name='gift' style={ styles.icon } />
+          </Button>
+          <Button>
+            <Icon name='transgender-alt' style={ styles.icon } />
+          </Button>
+        </Fab>
+        <ModalRedeem
+          visible={this.state.invisible}
+          onChangeCode={text => this.handleInputChange('code', text)}
+          onSubmit={() => this.placeRedeem()}
+          onModalPress={() => this.setState({ invisible: !this.state.invisible })}
+        />
       </Container>
     );
   }
 }
 
-export default connect(null, null)(Feed);
+const mapStateToProps = createStructuredSelector({
+  inputFields: selectors.getInputFields()
+});
+
+export default connect(mapStateToProps, actions)(Feed);
