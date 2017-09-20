0b5ede8f23f28b06e0c1c1f816082e1ef60933eb
kristoforusrp <kristoforusrp@gmail.com>
2017-09-21 02:08:21 +0700

    add constant for clear field

--- app/containers/Feed/constants.js
+++ app/containers/Feed/constants.js
@@ -4,3 +4,4 @@ export const POST_FEEDS = 'app/containers/Feed/POST_FEEDS';
 export const IS_POST_FEEDS = 'app/containers/Feed/IS_POST_FEEDS';
 export const UPDATE_IMAGE = 'app/containers/Feed/UPDATE_IMAGE';
 export const UPDATE_TEXT = 'app/containers/Feed/UPDATE_TEXT';
+export const CLEAR_FIELDS = 'app/containers/Feed/CLEAR_FIELDS';
