ad1185b9fac490f9e9f42cd0b597fb73333611ec
kristoforusrp <kristoforusrp@gmail.com>
2017-09-21 01:37:41 +0700

    handle update image and text

--- app/containers/Feed/reducer.js
+++ app/containers/Feed/reducer.js
@@ -3,12 +3,16 @@ import {
   FETCH_FEEDS,
   IS_FETCHING_FEEDS,
   POST_FEEDS,
-  IS_POST_FEEDS
+  IS_POST_FEEDS,
+  UPDATE_IMAGE,
+  UPDATE_TEXT
 } from './constants';
 
 const initialState = fromJS({
   feeds: [],
   postFeed: {},
+  image: {},
+  message: '',
   isFetching: false,
   isPosting: false
 });
@@ -28,6 +32,12 @@ function feedReducer(state = initialState, action) {
 
     case POST_FEEDS:
       return state.set('postFeed', fromJS(action.payloads));
+
+    case UPDATE_IMAGE:
+      return state.set('image', fromJS(action.image));
+
+    case UPDATE_TEXT:
+      return state.set('message', action.value);
 
     default:
       return state;
