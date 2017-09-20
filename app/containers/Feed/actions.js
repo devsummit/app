351770a0677a9c3109e864cc0ab1e67e7cc0d640
rendisaepul <rendisaepulbahri09@gmail.com>
2017-09-20 22:21:03 +0700

    add function/action for redem code

---
+++ app/containers/Feed/actions.js
@@ -0,0 +1,31 @@
+import {
+    DevSummitAxios,
+    getAccessToken
+} from '../../helpers';
+
+import local from '../../../config/local';
+import { UPDATE_SINGLE_INPUT_FIELD } from './constants';
+
+export function updateInputFields(field, value) {
+    return {
+        type: UPDATE_SINGLE_INPUT_FIELD,
+        field,
+        value
+    };
+}
+
+export function placeRedeem () {
+    return (dispatch, getState) => {
+        const { inputFields } = getState().get('code').toJS();
+        const { code } = inputFields;
+
+        getAccessToken()
+            .then((token) => {
+                DevSummitAxios.patch('api/v1/redeemcodes', { code },
+                { headers: {Authorization: token} })
+                .then((res) => {
+                    console.log('response code', res);
+                });
+            });
+    };
+}
