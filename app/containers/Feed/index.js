df52d8895eff164a1c418aa26a661338befcc5d3
caudaganesh <caudaganesh@gmail.com>
2017-09-21 02:43:57 +0700

    add notification button

--- app/containers/Feed/index.js
+++ app/containers/Feed/index.js
@@ -7,7 +7,8 @@ import {
   Text,
   Tabs,
   Tab,
-  TabHeading
+  TabHeading,
+  Fab
 } from 'native-base';
 import PropTypes from 'prop-types';
 import { RefreshControl, View } from 'react-native';
@@ -16,7 +17,7 @@ import { connect } from 'react-redux';
 import { Actions } from 'react-native-router-flux';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from './styles';
-import {PRIMARYCOLOR} from '../../constants';
+import { PRIMARYCOLOR } from '../../constants';
 import HeaderPoint from '../../components/Header';
 
 import TicketList from '../TicketList';
@@ -34,96 +35,102 @@ class Feed extends Component {
       <Container
         style={styles.container}
       >
-      <Content>
-        <HeaderPoint title="FEED" />
-        <Tabs style={styles.tabs}>
-          <Tab heading={<TabHeading style={styles.tabHeading}><Text style={styles.tabTitle}>News feed</Text></TabHeading>}>
-            <ListItem>
-              <Text style={styles.text}>Feed 1</Text>
-              <Button
-                small
-                style={styles.button}
-                onPress={() => {
-                  Actions.attendeesList({ ticketId: item.id });
-                }}
-              >
-                <Text style={styles.buttonText}>Share</Text>
-                <Icon
-                  name="share"
-                  color="white"
-                />
-              </Button>
-            </ListItem>
-            <ListItem>
-              <Text style={styles.text}>Feed 2</Text>
-              <Button
-                small
-                style={styles.button}
-                onPress={() => {
-                  Actions.attendeesList({ ticketId: item.id });
-                }}
-              >
-                <Text style={styles.buttonText}>Share</Text>
-                <Icon
-                  name="share"
-                  color="white"
-                />
-              </Button>
-            </ListItem>
-            <ListItem>
-              <Text style={styles.text}>Feed 3</Text>
-              <Button
-                small
-                style={styles.button}
-                onPress={() => {
-                  Actions.attendeesList({ ticketId: item.id });
-                }}
-              >
-                <Text style={styles.buttonText}>Share</Text>
-                <Icon
-                  name="share"
-                  color="white"
-                />
-              </Button>
-            </ListItem>
-            <ListItem>
-              <Text style={styles.text}>Feed 4</Text>
-              <Button
-                small
-                style={styles.button}
-                onPress={() => {
-                  Actions.attendeesList({ ticketId: item.id });
-                }}
-              >
-                <Text style={styles.buttonText}>Share</Text>
-                <Icon
-                  name="share"
-                  color="white"
-                />
-              </Button>
-            </ListItem>
-            <ListItem>
-              <Text style={styles.text}>Feed 5</Text>
-              <Button
-                small
-                style={styles.button}
-                onPress={() => {
-                  Actions.attendeesList({ ticketId: item.id });
-                }}
-              >
-                <Text style={styles.buttonText}>Share</Text>
-                <Icon
-                  name="share"
-                  color="white"
-                />
-              </Button>
-            </ListItem>
-          </Tab>
-          <Tab heading={<TabHeading style={styles.tabHeading}><Text style={styles.tabTitle}>Ticket</Text></TabHeading>}>
-            <TicketList />
-          </Tab>
-        </Tabs>
+        <Content>
+          <HeaderPoint title="FEED" />
+          <Tabs style={styles.tabs}>
+            <Tab heading={<TabHeading style={styles.tabHeading}><Text style={styles.tabTitle}>News feed</Text></TabHeading>}>
+              <ListItem>
+                <Text style={styles.text}>Feed 1</Text>
+                <Button
+                  small
+                  style={styles.button}
+                  onPress={() => {
+                    Actions.attendeesList({ ticketId: item.id });
+                  }}
+                >
+                  <Text style={styles.buttonText}>Share</Text>
+                  <Icon
+                    name="share"
+                    color="white"
+                  />
+                </Button>
+              </ListItem>
+              <ListItem>
+                <Text style={styles.text}>Feed 2</Text>
+                <Button
+                  small
+                  style={styles.button}
+                  onPress={() => {
+                    Actions.attendeesList({ ticketId: item.id });
+                  }}
+                >
+                  <Text style={styles.buttonText}>Share</Text>
+                  <Icon
+                    name="share"
+                    color="white"
+                  />
+                </Button>
+              </ListItem>
+              <ListItem>
+                <Text style={styles.text}>Feed 3</Text>
+                <Button
+                  small
+                  style={styles.button}
+                  onPress={() => {
+                    Actions.attendeesList({ ticketId: item.id });
+                  }}
+                >
+                  <Text style={styles.buttonText}>Share</Text>
+                  <Icon
+                    name="share"
+                    color="white"
+                  />
+                </Button>
+              </ListItem>
+              <ListItem>
+                <Text style={styles.text}>Feed 4</Text>
+                <Button
+                  small
+                  style={styles.button}
+                  onPress={() => {
+                    Actions.attendeesList({ ticketId: item.id });
+                  }}
+                >
+                  <Text style={styles.buttonText}>Share</Text>
+                  <Icon
+                    name="share"
+                    color="white"
+                  />
+                </Button>
+              </ListItem>
+              <ListItem>
+                <Text style={styles.text}>Feed 5</Text>
+                <Button
+                  small
+                  style={styles.button}
+                  onPress={() => {
+                    Actions.attendeesList({ ticketId: item.id });
+                  }}
+                >
+                  <Text style={styles.buttonText}>Share</Text>
+                  <Icon
+                    name="share"
+                    color="white"
+                  />
+                </Button>
+              </ListItem>
+            </Tab>
+            <Tab heading={<TabHeading style={styles.tabHeading}><Text style={styles.tabTitle}>Ticket</Text></TabHeading>}>
+              <TicketList />
+            </Tab>
+          </Tabs>
         </Content>
+        <Fab
+          style={{ backgroundColor: '#FFA726' }}
+          position="topRight"
+          onPress={() => Actions.notification()}>
+          <Icon name="bell" />
+        </Fab>
       </Container>
     );
   }
