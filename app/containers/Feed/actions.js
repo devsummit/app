7b1b765a223b8bdecb1b0e896698bef834971f20
kristoforusrp <kristoforusrp@gmail.com>
2017-09-21 22:13:47 +0700

    modify action to accept websocket payloads

--- app/containers/Feed/actions.js
+++ app/containers/Feed/actions.js
@@ -1,12 +1,12 @@
 import FormData from 'FormData';
 import { Platform } from 'react-native';
+
 import {
   FETCH_FEEDS,
   IS_FETCHING_FEEDS,
-  POST_FEEDS,
-  IS_POST_FEEDS,
   UPDATE_IMAGE,
   UPDATE_TEXT,
+  UPDATE_FEEDS,
   CLEAR_FIELDS
 } from './constants';
 
@@ -15,47 +15,60 @@ import {
   getAccessToken
 } from '../../helpers';
 
-export function fetchFeeds() {
-
-  return (dispatch) => {
-
-    dispatch(isFetchingFeeds(true))
-
-    getAccessToken()
-    .then((token) => {
-
-      DevSummitAxios.get('/api/v1/feeds', { headers: { Authorization: token } })
-        .then((response) => {
-
-          const payloads = response.data.data;
-
-          dispatch({ type: FETCH_FEEDS, payloads });
-
-          dispatch(isFetchingFeeds(false));
-
-        })
-        .catch(err => console.log(err))
-
-    });
-  }
-
+/**
+ * Receiver callback from container and send it to reducer
+ * for updating feeds
+ */
+export function updateFeeds(payload) {
+  const data = JSON.parse(payload);
+  const payloads = data.data;
+  return {
+    type: UPDATE_FEEDS,
+    payloads
+  };
 }
+
 
 export function isFetchingFeeds(status) {
   return {
     type: IS_FETCHING_FEEDS,
     status
-  }
+  };
+}
+
+export function fetchFeeds() {
+  return (dispatch) => {
+    dispatch(isFetchingFeeds(true));
+
+    getAccessToken()
+      .then((token) => {
+        DevSummitAxios.get('/api/v1/feeds', { headers: { Authorization: token } })
+          .then((response) => {
+            const payloads = response.data.data;
+
+            dispatch({ type: FETCH_FEEDS, payloads });
+
+            dispatch(isFetchingFeeds(false));
+          })
+          .catch((err) => {
+            console.log(err);
+            dispatch(isFetchingFeeds(false));
+          });
+      });
+  };
+}
+
+export function isPostFeeds(status) {
+  return {
+    type: IS_FETCHING_FEEDS,
+    status
+  };
 }
 
 export function postFeeds(image, text) {
   return (dispatch) => {
-
-    // dispatch(isPostFeeds(true));
-
     getAccessToken()
       .then((token) => {
-
         const form = new FormData();
 
         if (Platform.OS === 'ios') {
@@ -74,46 +87,29 @@ export function postFeeds(image, text) {
           form.append('message', text);
         }
 
-        const headers = { Authorization: token }
-
+        const headers = { Authorization: token };
 
         DevSummitAxios.post('api/v1/feeds', form, { headers })
           .then((response) => {
-
-            const payloads = response.data.data;
-
-            /**
-             * @todo integrate with socket.io
-             */
-            // dispatch({ type: POST_FEEDS, payloads});
-
-            dispatch({ type: CLEAR_FIELDS })
+            dispatch({ type: CLEAR_FIELDS, response });
 
             dispatch(isPostFeeds(false));
-
           })
-          .catch(err => console.log(err))
+          .catch(err => console.log(err));
       });
-  }
-}
-
-export function isPostFeeds(status) {
-  return {
-    type: IS_FETCHING_FEEDS,
-    status
-  }
+  };
 }
 
 export function updateImage(image) {
   return {
     type: UPDATE_IMAGE,
     image
-  }
+  };
 }
 
 export function updateText(value) {
   return {
     type: UPDATE_TEXT,
     value
-  }
+  };
 }
