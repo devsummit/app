cb343499364c8d485da11d6f274b0eeae7957fc3
caudaganesh <caudaganesh@gmail.com>
2017-09-22 20:03:42 +0700

    #260 handle refresh token

--- app/containers/MainTabs/PushNotification.js
+++ app/containers/MainTabs/PushNotification.js
@@ -1,87 +1,122 @@
-import React, { Component } from "react";
+import { Component } from 'react';
 
-import { Platform } from 'react-native';
+import { Platform, AppState, AsyncStorage, Alert } from 'react-native';
 
-import FCM, {FCMEvent, RemoteNotificationResult, WillPresentNotificationResult, NotificationType} from "react-native-fcm";
+import FCM, { FCMEvent, RemoteNotificationResult, WillPresentNotificationResult, NotificationType } from 'react-native-fcm';
 
+import { getAccessToken, DevSummitAxios } from '../../helpers';
+
+
+// this shall be called regardless of app state: running, background or not running.
+// Won't be called when app is killed by user in iOS
+
+FCM.on(FCMEvent.Notification, async (notif) => {
+  // there are two parts of notif. notif.notification contains the notification payload,
+  // notif.data contains data payload
+  if (notif.local_notification) {
+    // this is a local notification
+  }
+  if (notif.opened_from_tray) {
+    // app is open/resumed because user clicked banner
+  }
+  // await someAsyncCall();
+
+  if (Platform.OS === 'ios') {
+    // optional
+    // iOS requires developers to call completionHandler to end notification process.
+    // If you do not call it your background remote notifications could be throttled,
+    // to read more about it see the above documentation link.
+    // This library handles it for you automatically with default behavior
+    // (for remote notification, finish with NoData; for WillPresent,
+    // finish depend on "show_in_foreground"). However if you want to return different result,
+    // follow the following code to override
+    // notif._notificationType is available for iOS platfrom
+    switch (notif._notificationType) {
+      case NotificationType.Remote:
+        // other types available: RemoteNotificationResult.NewData,
+        // RemoteNotificationResult.ResultFailed
+        notif.finish(RemoteNotificationResult.NewData);
+        break;
+      case NotificationType.NotificationResponse:
+        notif.finish();
+        break;
+      case NotificationType.WillPresent:
+        // other types available: WillPresentNotificationResult.None
+        notif.finish(WillPresentNotificationResult.All);
+        break;
+      default:
+        break;
+    }
+  }
+});
+
+FCM.on(FCMEvent.RefreshToken, (token) => {
+  getAccessToken().then((usertoken) => {
+    const headers = { Authorization: usertoken };
+    DevSummitAxios.patch('auth/me/updatefcmtoken', { token }, { headers })
+      .then(async (response) => {
+        if (response.meta && response.meta.success) {
+          await AsyncStorage.setItem('fcmtoken', token);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }).catch((err) => {
+    console.log(err);
+  });
+  // fcm token may not be available on first load, catch it here
+});
 
 export default class PushNotification extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      currentTab: 0,
+      roleId: null,
+      fcm_token: '',
+      appState: AppState.currentState
+    };
   }
 
-  async componentDidMount() {
-
-    try{
-      let result = await FCM.requestPermissions({badge: true, sound: true, alert: true});
-    } catch(e){
-      console.error(e);
-    }
-
-    FCM.getFCMToken().then(token => {
-      this.props.onChangeToken(token);
-    });
-
+  componentDidMount() {
+    AppState.addEventListener('change', this.handleAppStateChange);
     FCM.subscribeToTopic('devsummit_indonesia_2017');
-
-    if(Platform.OS === 'ios'){
-      FCM.getAPNSToken().then(token => {
+    FCM.requestPermissions();
+    FCM.getFCMToken().then((token) => {
+      this.setState({ fcm_token: token });
+      // update your fcm token on server.
+      getAccessToken().then((usertoken) => {
+        const headers = { Authorization: usertoken };
+        DevSummitAxios.patch('auth/me/updatefcmtoken', { token }, { headers })
+          .then(async (response) => {
+            if (response.meta && response.meta.success) {
+              await AsyncStorage.setItem('fcmtoken', token);
+            }
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }).catch((err) => {
+        console.log(err);
       });
-    }
-
-    FCM.getInitialNotification().then(notif => {
     });
-
-    FCM.getScheduledLocalNotifications().then(notif=>console.log(notif));
-
-    this.notificationListener = FCM.on(FCMEvent.Notification, notif => {
-      if(notif.local_notification){
-        return;
+    this.notificationListener = FCM.on(FCMEvent.Notification, async (notif) => {
+      // do some component related stuff
+      if (this.state.appState === 'active') {
+        Alert.alert(notif.fcm.body);
       }
-      if(notif.opened_from_tray){
-        return;
-      }
-
-      if(Platform.OS ==='ios'){
-              //optional
-              //iOS requires developers to call completionHandler to end notification process. If you do not call it your background remote notifications could be throttled, to read more about it see the above documentation link.
-              //This library handles it for you automatically with default behavior (for remote notification, finish with NoData; for WillPresent, finish depend on "show_in_foreground"). However if you want to return different result, follow the following code to override
-              //notif._notificationType is available for iOS platfrom
-              switch(notif._notificationType){
-                case NotificationType.Remote:
-                  notif.finish(RemoteNotificationResult.NewData) //other types available: RemoteNotificationResult.NewData, RemoteNotificationResult.ResultFailed
-                  break;
-                case NotificationType.NotificationResponse:
-                  notif.finish();
-                  break;
-                case NotificationType.WillPresent:
-                  notif.finish(WillPresentNotificationResult.All) //other types available: WillPresentNotificationResult.None
-                  break;
-              }
-      }
-
-      this.refreshTokenListener = FCM.on(FCMEvent.RefreshToken, token => {
-        console.log("TOKEN (refreshUnsubscribe)", token);
-        this.props.onChangeToken(token);
-      });
-
-      // direct channel related methods are ios only
-      // directly channel is truned off in iOS by default, this method enables it
-      FCM.enableDirectChannel();
-      this.channelConnectionListener = FCM.on(FCMEvent.DirectChannelConnectionChanged, (data) => {
-        console.log('direct channel connected' + data);
-      });
-      setTimeout(function() {
-        FCM.isDirectChannelEstablished().then(d => console.log(d));
-      }, 1000);
-    })
+    });
   }
 
   componentWillUnmount() {
+    AppState.removeEventListener('change', this.handleAppStateChange);
     this.notificationListener.remove();
-    this.refreshTokenListener.remove();
   }
 
+  handleAppStateChange = (nextAppState) => {
+    this.setState({ appState: nextAppState });
+  }
 
   render() {
     return null;
