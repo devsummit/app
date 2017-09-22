6240d716668cc76cfdbb901e46ebe34450d0447e
akbar <akbar_rachman@ymail.com>
2017-09-23 03:25:15 +0700

    add reducers for upload

--- app/containers/BoothInfo/reducer.js
+++ app/containers/BoothInfo/reducer.js
@@ -4,6 +4,7 @@ import {
   UPDATE_FIELD,
   UPDATE_BOOTH_PHOTO,
   UPDATE_IS_BOOTH_PHOTO_UPDATED,
+  UPDATE_IS_BOOTH_GALLERY_UPDATED,
   FETCH_BOOTH_INFO
 } from './constants';
 
@@ -13,7 +14,8 @@ const initialState = fromJS({
   },
   boothGalleries: [],
   boothPhoto: 'https://museum.wales/media/40374/thumb_480/empty-profile-grey.jpg',
-  isBoothPhotoUpdated: false
+  isBoothPhotoUpdated: false,
+  isBoothGalleryUpdated: false
 });
 
 function boothInfoReducer(state = initialState, action) {
@@ -24,6 +26,8 @@ function boothInfoReducer(state = initialState, action) {
       return state.setIn([ 'fields', action.fields ], action.value);
     case UPDATE_IS_BOOTH_PHOTO_UPDATED:
       return state.set('isBoothPhotoUpdated', action.status);
+    case UPDATE_IS_BOOTH_GALLERY_UPDATED:
+      return state.set('isBoothGalleryUpdated', action.status);
     case FETCH_BOOTH_INFO:
       return state.set('boothGalleries', action.payloads);
     default:
