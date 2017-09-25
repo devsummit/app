eab09814898363f45eefe3ca02cedc625480138c
Mochamad Gufron <mgufronefendi@gmail.com>
2017-09-26 00:47:22 +0700

    add fastlane and config for development

---
+++ config/development.js
@@ -0,0 +1,15 @@
+export default {
+  API_BASE_URL: 'http://api.devsummit.io:8081',
+  CLIENT_SECRET: 'supersecret',
+
+  FB_CLIENT_ID: '216608565531165',
+  FB_CLIENT_SECRET: '0463ed52bd8a400dd48d8e9cc246acc4',
+
+  TWITTER_CALLBACK: 'devsummit://authorize',
+  TWITTER_CONSUMER_KEY: 'iJoptl48l8j5OseOI1lrS3r9N',
+  TWITTER_CONSUMER_KEY_SECRET: 'eJBRnVvE0YplptEelYJOuHYw2YLdOf9v39YNnfdM6Rkv3kNShC',
+
+  GOOGLE_CALLBACK_URL: 'http://localhost/google',
+  GOOGLE_CLIENT_ID: '1091376735288-sgpfaq0suha3qakagrsig7bee58enkqr.apps.googleusercontent.com',
+  GOOGLE_CLIENT_SECRET: 'ZdbNXvmMTy9dcAK8oW-3QPOj'
+};
