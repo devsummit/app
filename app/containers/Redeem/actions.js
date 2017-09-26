0457cb5b25bba8f6718bb7807d9fcee55f5b50e1
Arnaz <arnazmursidp.arnazmursid@gmail.com>
2017-09-26 22:44:24 +0700

    add actions for change data async storage

--- app/containers/Redeem/actions.js
+++ app/containers/Redeem/actions.js
@@ -1,32 +1,61 @@
 import {
-    DevSummitAxios,
-    getAccessToken
+  DevSummitAxios,
+  getAccessToken,
+  getProfileData,
+  getRoleId
 } from '../../helpers';
-import { Alert } from 'react-native';
+import { Alert, AsyncStorage } from 'react-native';
 
 import local from '../../../config/local';
 import { UPDATE_SINGLE_INPUT_FIELD } from './constants';
 
 export function updateInputFields(field, value) {
-    return {
-        type: UPDATE_SINGLE_INPUT_FIELD,
-        field,
-        value
-    };
+  return {
+    type: UPDATE_SINGLE_INPUT_FIELD,
+    field,
+    value
+  };
 }
 
-export function placeRedeem () {
-    return (dispatch, getState) => {
-        const { inputFields } = getState().get('code').toJS();
-        const { code } = inputFields;
+export function updateDataStorage(res) {
+  getProfileData()
+    .then(() => {
+      let data = JSON.stringify(res.data);
+      data = '';
+      AsyncStorage.removeItem('profile_data', () => {
+        try {
+          AsyncStorage.setItem('profile_data', data);
+        } catch (e) {
+          console.log('ERROR', e);
+        }
+      });
+    });
+  getRoleId()
+    .then(() => {
+      AsyncStorage.removeItem('role_id', () => {
+        try {
+          AsyncStorage.setItem('role_id', 3);
+        } catch (e) {
+          console.log('ERROR', e);
+        }
+      });
+    });
+}
 
-        getAccessToken()
-            .then((token) => {
-                DevSummitAxios.patch('api/v1/redeemcodes', { code },
-                { headers: {Authorization: token} })
-                .then((res) => {
-                    Alert.alert('Information', res.data.meta.message);
-                });
-            });
-    };
+export function placeRedeem() {
+  return (dispatch, getState) => {
+    const { inputFields } = getState().get('code').toJS();
+    const { code } = inputFields;
+
+    getAccessToken()
+      .then((token) => {
+        DevSummitAxios.patch('api/v1/redeemcodes', { code },
+          { headers: { Authorization: token } })
+          .then((res) => {
+            updateDataStorage(res);
+            console.log('RESOPSIKNMONK', res);
+            Alert.alert('Information', res.data.meta.message);
+          }).catch((error) => { console.log('ERROR', error) });
+      });
+  };
 }
