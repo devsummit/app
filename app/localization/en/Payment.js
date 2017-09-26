ea4771a81347b803a89f828a00a75468211bcd5c
akbar <akbar_rachman@ymail.com>
2017-09-27 05:10:34 +0700

    #277 - add payment localization

---
+++ app/localization/en/Payment.js
@@ -0,0 +1,22 @@
+export default {
+  // Payment
+  method: 'Payment method',
+  bank: 'Bank',
+  goToPaymentDetail: 'Go to payment detail',
+
+  // Payment detail
+  email: 'Email',
+  firstName: 'First name',
+  lastName: 'Last name',
+  phone: 'Phone number',
+  vaNumber: 'VA number',
+  cardNumber: 'Card number',
+  expiry: 'Expiry (MM/YYYY)',
+  expiryMonth: 'Expiry month',
+  expiryYear: 'Expiry year',
+  description: 'Description',
+  digitsNumber: '16 digits card number',
+  lastDigitsNumber: 'Last 10 digits',
+  totalAmount: 'Total amount',
+  submitPayment: 'Submit payment'
+}
