ca22a6609087a119f64a510e95a6b1564c9e37ed
akbar <akbar_rachman@ymail.com>
2017-09-27 06:08:18 +0700

    #277 - update order localization for ticket list

--- app/localization/en/Order.js
+++ app/localization/en/Order.js
@@ -22,5 +22,14 @@ export default {
   instruction1: 'Please pay the total bill',
   instruction2: 'to account number',
   instruction3: 'from Veritrans',
-  attention: 'Important: the payment deadline is 1 (one) hour. If the deadline exceeded then your order will be automatically canceled.'
+  attention: 'Important: the payment deadline is 1 (one) hour. If the deadline exceeded then your order will be automatically canceled.',
+
+  // Ticket list
+  ticketNumber: 'Ticket No.',
+  noTicket: 'You have no tickets, please order or try to refresh',
+  refresh: 'Refresh',
+  myOrder: 'My orders',
+  pending: 'orders is pending',
+  allTicket: 'All your ticket orders shows up here',
+  ticketOrder: 'Ticket orders'
 };
