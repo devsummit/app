b24aa94fd612cd73e3dcb89857718918eabd5ebd
akbar <akbar_rachman@ymail.com>
2017-09-27 19:35:34 +0700

    fix typo

--- app/localization/en/Feed.js
+++ app/localization/en/Feed.js
@@ -1,7 +1,7 @@
 export default {
   title: 'FEED',
   newsFeed: 'News feed',
-  shareActvity: 'Share your activity ...',
+  shareActivity: 'Share your activity ...',
   post: 'Post',
   share: 'Share',
   showMore: 'Show more feeds',
--- app/localization/id/Feed.js
+++ app/localization/id/Feed.js
@@ -1,7 +1,7 @@
 export default {
   title: 'FEED',
   newsFeed: 'News feed',
-  shareActvity: 'Bagikan aktifitas anda ...',
+  shareActivity: 'Bagikan aktifitas anda ...',
   post: 'Kirim',
   share: 'Bagikan',
   showMore: 'Tampilkan lebih banyak',
