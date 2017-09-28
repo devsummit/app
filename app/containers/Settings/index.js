40e2311902745c5aacaa3501fa24738ce5ea0cb7
akbar <akbar_rachman@ymail.com>
2017-09-28 19:08:09 +0700

    remove modal

--- app/containers/Settings/index.js
+++ app/containers/Settings/index.js
@@ -4,7 +4,7 @@ import {
   Content,
   Text
 } from 'native-base';
-import { View, ScrollView, Modal } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 
 // import redux componens
@@ -64,7 +64,7 @@ class Settings extends Component {
                   block
                   rounded
                   style={styles.button}
-                  onPress={() => { this.setModalVisible(true); }}
+                  onPress={() => Actions.codeConduct()}
                 >
                   <Text>{strings.settings.codeConduct}</Text>
                 </Button>
@@ -77,45 +77,6 @@ class Settings extends Component {
                 >
                   <Text>{strings.settings.logout}</Text>
                 </Button>
-              </View>
-              <View style={{ marginTop: 22 }}>
-                <Modal
-                  animationType="fade"
-                  transparent={false}
-                  visible={this.state.modalVisible}
-                  onRequestClose={() => { this.setModalVisible(!this.state.modalVisible); }}
-                >
-                  <ScrollView>
-                    <View style={{ marginTop: 22, marginLeft: 22, marginRight: 22, marginBottom: 22 }}>
-                      <View>
-                        <Text style={{ fontSize: 20, textAlign: 'center', color: '#000', fontWeight: 'bold' }}>Code of Conduct :</Text>
-                        <Text />
-                        <Text style={{ fontSize: 16, textAlign: 'left', color: '#000' }}>DevSummit is a tech event intended for networking and collaboration in the tech community. We value the participation of each member and want all attendees to have a meaningful and memorable experience.</Text>
-                        <Text />
-                        <Text style={{ fontSize: 16, textAlign: 'left', color: '#000' }}>All attendees are expected to show respect and courtesy to other attendees throughout all events hosted in the Sabuga, Bandung, Indonesia. To make clear what is expected, all attendees, speakers, exhibitors, organizers, and volunteers at any event in the Sabuga, Bandung, Indonesia are required to conform to the following Code of Conduct. Organizers will enforce this code throughout the event.</Text>
-                        <Text />
-                        <Text style={{ fontSize: 16, textAlign: 'left', color: '#000' }}>DevSummit is dedicated to providing a harassment-free event experience for everyone, regardless of gender, disability, physical appearance, body size, race, or religion. We do not tolerate harassment of event participants in any form, nor do we tolerate any behavior that would reasonably lead to another event participant being made to feel unsafe, insecure, or frightened for their physical or emotional well-being.</Text>
-                        <Text />
-                        <Text style={{ fontSize: 16, textAlign: 'left', color: '#000' }}>All communication should be appropriate for a professional audience including people of many different backgrounds. Sexual language and imagery is not appropriate for any presentation given in the Sabuga, Bandung, Indonesia or in any presentation associated with or related to DevSummit. Be kind to others. Do not insult or put down other attendees.</Text>
-                        <Text />
-                        <Text style={{ fontSize: 16, textAlign: 'left', color: '#000' }}>Each event guest is entirely responsible for his or her own actions.</Text>
-                        <Text />
-                        <Text style={{ fontSize: 16, textAlign: 'left', color: '#000' }}>Attendees violating these rules will be asked to leave the event at the sole discretion of the event managers.</Text>
-                        <Text />
-                        <Text style={{ fontSize: 16, textAlign: 'center', color: '#000', fontWeight: 'bold' }}>Thank you for your participation.</Text>
-                        <Button
-                          primary
-                          style={styles.btnModal}
-                          onPress={() => {
-                            this.setModalVisible(!this.state.modalVisible);
-                          }}
-                        >
-                          <Text style={{ color: '#FFF' }}>{strings.global.back}</Text>
-                        </Button>
-                      </View>
-                    </View>
-                  </ScrollView>
-                </Modal>
               </View>
             </Content>
           </ScrollView>
