1704bf4ddf5f4244310469c3fdac0781efb52ce3
rendisaepul <rendisaepulbahri09@gmail.com>
2017-09-28 11:37:23 +0700

    add const content report

--- app/containers/Feed/constants.js
+++ app/containers/Feed/constants.js
@@ -9,3 +9,28 @@ export const UPDATE_FEEDS = 'app/containers/Feed/UPDATE_FEEDS';
 export const CLEAR_IMAGE = 'app/containers/Feed/CLEAR_IMAGE';
 export const UPDATE_CURRENT_PAGE = 'app/containers/Feed/UPDATE_CURRENT_PAGE';
 export const LOAD_MORE_FEEDS = 'app/containers/Feed/LOAD_MORE_FEEDS';
+export const REMOVE_FEED = 'app/containers/Feed/REMOVE_FEED';
+export const IS_REMOVE_FEED = 'app/containers/Feed/IS_REMOVE_FEED';
+
+export const CONTENT_REPORT = [
+    {
+        value: 'key0',
+        label: 'Spam or scam',
+        report: 'Spam'
+    },
+    {
+        value: 'key1',
+        label: 'Racism, hateful towards certain group',
+        report: 'Racism'
+    },
+    {
+        value: 'key2',
+        label: 'Pornography, sexually explicit content',
+        report: 'Pornography'
+    },
+    {
+        value: 'key3',
+        label: 'Violence or harmful behavior',
+        report: 'Violence'
+    }
+];
