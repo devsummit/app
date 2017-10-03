eac5c85465e398f98160ab62cedad180adad5c18
rendisaepul <rendisaepulbahri09@gmail.com>
2017-10-03 21:36:16 +0700

    styling

--- app/components/OrderItem/styles.js
+++ app/components/OrderItem/styles.js
@@ -1,10 +1,15 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'whitesmoke',
+    margin: 5,
+    elevation: 3,
+    borderRadius: 3
+  },
   item: {
     flex: 1,
-    justifyContent: 'flex-start',
-    flexDirection: 'column'
+    flexDirection: 'row',
   },
   left: {
     justifyContent: 'flex-start',
@@ -22,9 +27,9 @@ const styles = StyleSheet.create({
     alignItems: 'center'
   },
   icon: {
-    flex: 1,
-    marginLeft: 10,
-    fontSize: 30
+    fontSize: 25,
+    margin: 5,
+    color: '#FFD740'
   },
   buttonText: {
     marginRight: 10,
@@ -32,25 +37,45 @@ const styles = StyleSheet.create({
   },
   orderId: {
     alignSelf: 'flex-start',
-    fontWeight: 'bold'
   },
   text: {
-    alignSelf: 'flex-end'
+    alignSelf: 'flex-end',
+    marginRight: 3
   },
   btnCheckOut: {
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    marginTop: 6,
-    marginBottom: 6,
-    borderWidth: 1,
     borderRadius: 30,
     borderColor: 'transparent',
-    flexDirection: 'row',
     elevation: 0,
-    flex: 1
   },
-  card: {
-    flex: 0,
+  viewText: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-end'
+  },
+  status: {
+    alignSelf: 'flex-end',
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: 'white',
+    borderRadius: 3,
+    padding: 3,
+  },
+  buttonSection: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10
+  },
+  statusText: {
+    fontSize: 12,
+    borderRadius: 5,
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 3,
+    marginLeft: 10
   }
 });
 
