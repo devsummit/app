0dcc2a5695be5288f2c8b570c337901e775fddd2
akbar <akbar_rachman@ymail.com>
2017-10-05 16:02:05 +0700

    add image on code conduct

--- app/components/CodeConduct/index.js
+++ app/components/CodeConduct/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { View, ScrollView, Text } from 'react-native';
+import { View, ScrollView, Text, Image } from 'react-native';
 import Actions from 'react-native-router-flux';
 
 import Button from '../../components/Button';
 import styles from './style';
 import strings from '../../localization';
 
+const icon = require('./../../../assets/images/icon.png');
 
 const CodeConduct = () => {
   return (
     <ScrollView>
       <View style={styles.container}>
+        <View style={{ flex: 1, alignItems: 'center' }}>
+          <Image
+            source={icon}
+            resizeMode="center"
+          />
+          <Text style={styles.app}>Devsummit</Text>
+        </View>
         <Text style={styles.content}>{strings.codeConduct.content1} {'\n'} {'\n'}</Text>
         <Text style={styles.content}>{strings.codeConduct.content2} {'\n'} {'\n'}</Text>
         <Text style={styles.content}>{strings.codeConduct.content3} {'\n'} {'\n'}</Text>
