a9cb818dd8c54aacf7a25e3bb67b20db9a5175b3
andy-shi88 <shi88.andy@gmail.com>
2017-10-11 18:25:19 +0700

    move constants

--- app/containers/Settings/constants.js
+++ app/containers/Settings/constants.js
@@ -4,4 +4,3 @@ export const UPDATE_AVATAR = 'app/containers/Profile/UPDATE_AVATAR';
 export const UPDATE_IS_AVATAR_UPDATED = 'app/containers/Profile/UPDATE_IS_AVATAR_UPDATED';
 export const UPDATE_IS_LOG_OUT = 'app/containers/Profile/UPDATE_IS_LOG_OUT';
 export const UPDATE_IS_DISABLED = 'app/containers/Profile/UPDATE_IS_DISABLED';
-export const feedback = 'https://devsummit.io/feedback.html';
