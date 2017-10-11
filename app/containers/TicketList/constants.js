aec3748485ebbcefd1990f298774e2cad1060372
rendisaepul <rendisaepulbahri09@gmail.com>
2017-10-11 22:24:49 +0700

    add const

--- app/containers/TicketList/constants.js
+++ app/containers/TicketList/constants.js
@@ -3,3 +3,5 @@ const config = require('../../../config/local').default;
 export const FETCH_USER_TICKET = 'app/containers/TicketList/FETCH_USER_TICKET';
 export const IS_FETCHING_USER_TICKET = 'app/containers/TicketList/IS_FETCHING_USER_TICKET';
 export const FETCHING_USER_TICKET_STATUS = 'app/containers/TicketList/FETCHING_USER_TICKET_STATUS';
+export const UPDATE_INPUT_FIELDS = 'app/containers/TicketList/UPDATE_INPUT_FIELDS';
+export const IS_TRANSFER_TICKET = 'app/containers/TicketList/IS_TRANSFER_TICKET';
