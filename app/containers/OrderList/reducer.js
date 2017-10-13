a7067933ce990e960bb6fd90994527c77a19347a
Krisrp <kristoforusrp@gmail.com>
2017-10-13 19:19:19 +0700

    add reducer for free pass status and redeem

--- app/containers/OrderList/reducer.js
+++ app/containers/OrderList/reducer.js
@@ -8,7 +8,9 @@ import {
   IS_FETCHING_ORDERS,
   SET_CONFIRM_PAYMENT,
   IS_CONFIRMING_PAYMENT,
-  PENDING_ORDERS
+  PENDING_ORDERS,
+  REDEEM_COUNTER,
+  SUBMIT_REFERAL
 } from './constants';
 
 /*
@@ -18,7 +20,9 @@ const initialState = fromJS({
   orders: [],
   isFetchingOrders: false,
   isConfirmingPayment: false,
-  pendingOrder: 0
+  pendingOrder: 0,
+  redeemCounter: 0,
+  redeemStatus: false
 });
 
 function orderListReducer(state = initialState, action) {
@@ -36,6 +40,10 @@ function orderListReducer(state = initialState, action) {
       return state.set('isConfirmingPayment', action.status);
     case PENDING_ORDERS:
       return state.set('pendingOrder', action.value);
+    case REDEEM_COUNTER:
+      return state.set('redeemCounter', action.value);
+    case SUBMIT_REFERAL:
+      return state.set('redeemStatus', action.status);
     default:
       return state;
   }
