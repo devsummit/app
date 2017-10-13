a7161c477b6bca9fd9a6bd53347067e12dd2d8b7
Mochamad Gufron <mgufronefendi@gmail.com>
2017-10-14 00:42:56 +0700

    comment unnecessary code

--- app/containers/Payment/index.js
+++ app/containers/Payment/index.js
@@ -70,6 +70,49 @@ class Payment extends Component {
       <Container style={styles.container}>
         <Content>
           <Text style={styles.littleText}>{strings.payment.method}</Text>
+          {/* <View style={styles.pickerWrapper}>
+            <Picker
+              style={styles.picker}
+              mode="dropdown"
+              selectedValue={paymentType}
+              onValueChange={value => this.handleInputChange('paymentType', value)}
+            >
+              {PAYMENT_METHODS.map(component => (
+                <Item key={component.value} label={component.label} value={component.payment_type} />
+              ))}
+            </Picker>
+          </View>
+          {(paymentType === 'bank_transfer' || paymentType === 'credit_card') ?
+            <View>
+              <Text style={styles.littleText}>{strings.payment.bank}</Text>
+              <View style={styles.pickerWrapper}>
+                <Picker
+                  style={styles.picker}
+                  placeholder="Bank"
+                  mode="dropdown"
+                  selectedValue={bankDestination}
+                  onValueChange={value => this.handleInputChange('bankDestination', value)}
+                >
+                  {bankList.map(component => (
+                    <Item
+                      key={component.value}
+                      label={component.label}
+                      value={component.bankDestination}
+                    />
+                  ))}
+                </Picker>
+              </View>
+            </View> : <View />}
+          <Button
+            style={styles.button}
+            onPress={() => {
+              Actions.paymentDetail({ order });
+            }}
+          >
+            <Text>
+              {strings.payment.goToPaymentDetail}
+            </Text>
+          </Button> */}
           <Button
             style={styles.button}
             onPress={() => {
