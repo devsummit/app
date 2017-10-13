6fee0c7b6b3e0dcb0dd098dd8525249e5c9a135c
Krisrp <kristoforusrp@gmail.com>
2017-10-13 19:16:25 +0700

    create free pass ticket constant

--- app/containers/OrderList/constants.js
+++ app/containers/OrderList/constants.js
@@ -3,3 +3,5 @@ export const IS_FETCHING_ORDERS = 'app/containers/OrderList/IS_FETCHING_ORDERS';
 export const SET_CONFIRM_PAYMENT = 'app/containers/OrderList/SET_CONFIRM_PAYMENT';
 export const IS_CONFIRMING_PAYMENT = 'app/containers/OrderList/IS_CONFIRMING_PAYMENT';
 export const PENDING_ORDERS = 'app/containers/OrderList/PENDING_ORDERS';
+export const REDEEM_COUNTER = 'app/containers/OrderList/REDEEM_COUNTER';
+export const SUBMIT_REFERAL = 'app/containers/OrderListSUBMIT_REFERAL';
