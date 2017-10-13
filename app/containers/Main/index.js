fe868ed8a0ee5020818359ad0cc96f27cff24b77
denisenricohasyim93 <denisenricohasyim93@gmail.com>
2017-10-13 22:22:42 +0700

    create animation which is progress bar

--- app/containers/Main/index.js
+++ app/containers/Main/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Container, Content, Text, Spinner, Item, Input, Header } from 'native-base';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {
+  AppRegistry,
+  StyleSheet,
   Image,
   View,
   Alert,
@@ -10,7 +12,11 @@ import {
   KeyboardAvoidingView,
   ScrollView,
   Animated,
-  Keyboard
+  Keyboard,
+  Easing,
+  Modal,
+  TouchableHighlight,
+  TouchableOpacity
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import AccountKit, { LoginButton } from 'react-native-facebook-account-kit';
@@ -23,6 +29,13 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import PropTypes from 'prop-types';
 
+import * as Animatable from 'react-native-animatable';
+import * as Progress from 'react-native-progress';
+// import * as TouchableBounce from 'react-native-touchable-bounce';
+import Bounceable from "react-native-bounceable";
+// import PulseLoader from 'react-native-pulse-loader';
+
+
 import AuthLogo from '../../components/AuthLogo';
 import Button from '../../components/Button';
 import ModalComponent from '../../components/ModalComponent';
@@ -31,18 +44,92 @@ import styles, { IMAGE_HEIGHT, IMAGE_HEIGHT_SMALL } from './styles';
 import * as actions from './actions';
 import * as selectors from './selectors';
 
+// const Pulse = require('react-native-pulse');
+
 const Transition = createTransition(Fade);
 const background = require('./../../../assets/images/background.png');
 const Logo = require('../../../assets/images/logo.png');
+const Aiken = require('../../../assets/images/icon.png');
+const timing = 4000;
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.imageHeight = new Animated.Value(IMAGE_HEIGHT);
+    // this.spinValue = new Animated.Value(0);
   }
 
+  // setAnimation = (effect) => {
+  //   switch (effect) {
+  //     case 'scale':
+  //       this.setState({ animate: this.scaleValue, type: 'scale' })
+  //       this.scale()
+
+  //       break;
+
+  //     case 'moveit':
+  //       const margin300 = this.divideScale.interpolate({
+  //           inputRange: [0, 0.5, 1],
+  //           outputRange: [0, 300, 0]
+  //       })
+  //       this.setState({ movingMargin: margin300 })
+  //       this.moveit()
+
+  //       break;
+
+  //     case 'stopmove':
+  //       const movingMargin = this.divideScale.interpolate({
+  //           inputRange: [0, 0.5, 1],
+  //           outputRange: [0, 0, 0]
+  //       })
+  //       this.setState({ movingMargin: movingMargin })
+  //       this.stopmove()
+
+  //       break;
+
+  //     case 'spin':
+  //        const spin = this.spinValue.interpolate({
+  //           inputRange: [0, 1],
+  //           outputRange: ['0deg', '360deg']
+  //        })
+  //        this.setState({ animate: spin })
+  //        this.spin()
+
+  //        break;
+
+  //     case 'stopspin':
+  //       const stopSpin = this.spinValue.interpolate({
+  //         inputRange: [0, 1],
+  //         outputRange: ['0deg', '0deg']
+  //       })
+  //       this.setState({ animate: stopSpin })
+  //       this.stopSpin()
+
+  //       break;
+
+
+  //     default:
+  //       break;
+  //   }
+  // }
+
+  // moveit = () => {
+  //   this.divideScale.setValue(0.3)
+  //       Animated.timing(
+  //         this.divideScale,
+  //         {
+  //           toValue: 1,
+  //           duration: 1000,
+  //           easing: Easing.bounce
+  //         }
+  //       ).start(() => this.moveit())
+  // }
+
   state = {
-    modalVisible: false
+    modalVisible: false,
+    modalVisibleAnimation: false,
+    // fadeAnim: new Animated.Value(0),
+    // movingMargin: null
   };
 
   componentWillMount() {
@@ -74,6 +161,28 @@ class Main extends Component {
     this.keyboardWillHideSub.remove();
   }
 
+  // componentDidMount () {
+  //   this.spin();
+  //   Animated.timing(                  // Animate over time
+  //     this.state.fadeAnim,            // The animated value to drive
+  //     {
+  //       toValue: 1,                   // Animate to opacity: 1 (opaque)
+  //       duration: 10000,              // Make it take a while
+  //     }
+  //   ).start();
+  // }
+  // spin () {
+  //   this.spinValue.setValue(0)
+  //   Animated.timing(
+  //     this.spinValue,
+  //     {
+  //       toValue: 1,
+  //       duration: timing,
+  //       easing: Easing.linear
+  //     }
+  //   ).start(() => this.spin())
+  // }
+
   onLoginMobile(token) {
     if (!token) {
       this.setState({});
@@ -90,6 +199,10 @@ class Main extends Component {
 
   setModalVisible = () => {
     this.setState({ modalVisible: !this.state.modalVisible });
+  };
+
+  setModalVisibleAnimation = () => {
+    this.setState({ modalVisibleAnimation: !this.state.modalVisibleAnimation});
   };
 
   configureAccountKit = () => {
@@ -128,9 +241,27 @@ class Main extends Component {
   };
 
   render() {
+
+    // const movingMargin = this.spinValue.interpolate({
+    //   inputRange: [0, 0.5, 1],
+    //   outputRange: [-300, 300, 0]
+    // })
+
+    // let { fadeAnim } = this.state;
     const { fields, isLoading } = this.props;
     const { username, password, email } = fields || '';
     const visible = false;
+    // const spin = this.spinValue.interpolate({
+    //   inputRange: [0, 1],
+    //   outputRange: ['0deg', '360deg']
+    // })
+    /* This also works, to show functions instead of strings */
+    // const getStartValue = () => '0deg'
+    // const getEndValue = () => '360deg'
+    // const spin = this.spinValue.interpolate({
+    //  inputRange: [0, 1],
+    //  outputRange: [getStartValue(), getEndValue()]
+    //  })
     return (
       <KeyboardAvoidingView style={styles.container} behaviour="padding">
         <Image source={background} style={styles.background}>
@@ -185,12 +316,67 @@ class Main extends Component {
                     }}
                   >
                     {isLoading ? (
-                      <ActivityIndicator size={'large'} color={'#FFFFFF'} />
+                      <View>
+                        <ActivityIndicator size={'large'} color={'#FFFFFF'} />
+                      </View>
                     ) : (
-                      <Text>Log In</Text>
+                      <View>
+                        <Text>Log In</Text>
+                      </View>
                     )}
                   </Button>
                 )}
+
+                  <Modal
+                    animationType="slide"
+                    transparent={false}
+                    visible={isLoading}
+                    onRequestClose={() => {this.setModalVisibleAnimation(false)}}
+                    >
+                    <Image source={background} style={styles.background}>
+                    <View style={{margin: 150}}>
+                      <View style={{
+                          flex: 0,
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                        }}>
+                        {/* <Animated.Image
+                          source={Aiken}
+                          style={{ 
+                            height: this.imageHeight,
+                            opacity: this.state.fadeAnim, // Binds directly
+                            transform: [{
+                              translateY: this.state.fadeAnim.interpolate({
+                                inputRange: [0, 1],
+                                outputRange: [150, 0]  // 0 : 150, 0.5 : 75, 1 : 0
+                              }),
+                            }]
+                          }}
+                          resizeMode="contain"
+                        /> */}
+                        {/* <Pulse color='orange' numPulses={3} diameter={400} speed={20} duration={2000} /> */}
+                        <Bounceable
+                          onPress={()=>{this.onPress}}
+                          level={1.5}>
+                          <Animated.Image
+                            source={Aiken}
+                            style={{ 
+                              height: this.imageHeight,
+                            }}
+                            resizeMode="contain"
+                          />
+                        </Bounceable>
+                        <Progress.Bar indeterminate width={200} />
+                      </View>
+                    </View>
+                  </Image>
+                  </Modal>
+
+                  {/* <TouchableHighlight onPress={() => {
+                    this.setModalVisibleAnimation(true)
+                  }}>
+                    <Text>Show Modal</Text>
+                  </TouchableHighlight> */}
                 <View style={styles.lineSection}>
                   <View style={styles.lineTextThree} />
                   <Text style={styles.lineTextFour}> or </Text>
