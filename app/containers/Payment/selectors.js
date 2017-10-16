ef9d487dcf5ad84511a04ebe7c203b32d76b5151
Krisrp <kristoforusrp@gmail.com>
2017-10-17 06:14:19 +0700

    update reducer and selector for create order

--- app/containers/Payment/selectors.js
+++ app/containers/Payment/selectors.js
@@ -12,21 +12,18 @@ const selectPaymentMethodReducer = () => state => state.get('methodPayment');
  *  Selects the username field data
  *  Returns object
  */
-export const getInputFields = () => createSelector(
-  selectPaymentMethodReducer(),
-  state => state.get('inputFields').toJS()
-);
+export const getInputFields = () =>
+  createSelector(selectPaymentMethodReducer(), state => state.get('inputFields').toJS());
 
 /**
  *  Selects the error checkeing field data
  *  Returns object
  */
-export const getErrorFields = () => createSelector(
-  selectPaymentMethodReducer(),
-  state => state.get('errorFields').toJS()
-);
+export const getErrorFields = () =>
+  createSelector(selectPaymentMethodReducer(), state => state.get('errorFields').toJS());
 
-export const isPayingWithPaypal = () => createSelector(
-  selectPaymentMethodReducer(),
-  state => state.get('isPayingWithPaypal')
-);
+export const isPayingWithPaypal = () =>
+  createSelector(selectPaymentMethodReducer(), state => state.get('isPayingWithPaypal'));
+
+export const getTicketTypes = () =>
+  createSelector(selectPaymentMethodReducer(), state => state.get('ticketTypes').toJS());
