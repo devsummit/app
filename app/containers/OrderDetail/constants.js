119bec8c8afdba29c4a6af9595b06cebe9a57f33
Arnaz <arnazmursid@gmail.com>
2017-10-17 04:19:50 +0700

    add set order id constant

--- app/containers/OrderDetail/constants.js
+++ app/containers/OrderDetail/constants.js
@@ -7,3 +7,4 @@ export const RESET_STATE = 'app/containers/OrderDetail/UPDATE_ORDER_DETAIL';
 export const SET_CONFIRM_PAYMENT = 'app/containers/OrderDetail/SET_CONFIRM_PAYMENT';
 export const IS_CONFIRMING_PAYMENT = 'app/containers/OrderDetail/IS_CONFIRMING_PAYMENT';
 export const SET_PAYMENT_PROOF = 'app/containers/OrderDetail/SET_PAYMENT_PROOF';
+export const SET_ORDER_ID = 'app/containers/OrderDetail/SET_ORDER_ID';
