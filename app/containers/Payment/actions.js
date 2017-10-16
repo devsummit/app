90f3107c88ba4a169fca2f9a8325f4e579e454b3
Arnaz <arnazmursid@gmail.com>
2017-10-17 04:20:48 +0700

    add payment with bank transfer function

--- app/containers/Payment/actions.js
+++ app/containers/Payment/actions.js
@@ -1,4 +1,5 @@
 import PayPal from 'react-native-paypal';
+import Toast from 'react-native-simple-toast';
 import { DevSummitAxios, getAccessToken } from '../../helpers';
 
 /*
@@ -7,7 +8,9 @@ import { DevSummitAxios, getAccessToken } from '../../helpers';
 import {
   UPDATE_SINGLE_INPUT_FIELD,
   UPDATE_SINGLE_ERROR_FIELD,
-  IS_PAYING_WITH_PAYPAL
+  IS_PAYING_WITH_PAYPAL,
+  UPDATE_USER_ID,
+  UPDATE_ORDER
 } from './constants';
 
 /*
@@ -19,6 +22,14 @@ import {
   PAYPAL_RATE,
   PAYPAL_ENV
 } from '../../constants';
+
+// update user id
+export function updateUserId(value) {
+  return {
+    type: UPDATE_USER_ID,
+    value
+  };
+}
 
 /*
  * Update the input fields
@@ -57,10 +68,52 @@ export function isPayingWithPaypal(value) {
   };
 }
 
+// update order
+// export function updateOrder(value){
+//   return {
+//     type: UPDATE_ORDER,
+//     value
+//   }
+// }
+
 /*
  * Initiate payment with PayPal
  * @param {order: order data}
- */
+*/
+
+export function payWithBankTransfer(userId, order, referalCode, callback = () => ({})) {
+  return (dispatch) => {
+    const orderItems = Object.keys(order).map((key) => { return order[key]; });
+    const data = {
+      user_id: userId,
+      order_details: orderItems,
+      referal_code: referalCode,
+      payment_type: 'bank_transfer'
+    };
+    getAccessToken()
+      .then((accessToken) => {
+        DevSummitAxios.post('api/v1/orders', data, {
+          headers: {
+            Authorization: accessToken,
+            'content-type': 'application/json'
+          }
+        })
+          .then((response) => {
+            if (response.data && response.data.meta.success) {
+              callback({
+                ...response.data.data,
+                ...response.data.included[0]
+              });
+            }
+          })
+          .catch((error) => {
+            Toast.show('Sorry, something went wrong');
+            console.log('ERROR', error);
+          });
+      });
+  };
+}
+
 export function payWithPaypal(order, callback = () => {}) {
   return (dispatch) => {
     dispatch(isPayingWithPaypal(true));
@@ -83,11 +136,9 @@ export function payWithPaypal(order, callback = () => {}) {
             headers: { Authorization: accessToken }
           });
         }).then((result) => {
-          console.log('result', result);
           dispatch(isPayingWithPaypal(false));
-          callback(result)
+          callback(result);
         }).catch((error) => {
-          console.log('error', error)
           dispatch(isPayingWithPaypal(false));
         });
     })
