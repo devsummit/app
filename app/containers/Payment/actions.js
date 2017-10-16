c9ef37e0b18990e60cb7d855a50ce2d0c9121ed4
Krisrp <kristoforusrp@gmail.com>
2017-10-17 06:13:15 +0700

    update payment to using service and create order

--- app/containers/Payment/actions.js
+++ app/containers/Payment/actions.js
@@ -1,24 +1,22 @@
 import PayPal from 'react-native-paypal';
 import { DevSummitAxios, getAccessToken } from '../../helpers';
-
+import payment from '../../services/payment';
+import { getOrderDetail } from '../OrderDetail/actions';
 /*
  * import constants
  */
 import {
   UPDATE_SINGLE_INPUT_FIELD,
   UPDATE_SINGLE_ERROR_FIELD,
-  IS_PAYING_WITH_PAYPAL
+  IS_PAYING_WITH_PAYPAL,
+  GET_TICKET_TYPES,
+  CREATE_ORDER
 } from './constants';
 
 /*
  * import Paypal Constants
  */
-import {
-  PAYPAL_CLIENT_ID,
-  PAYPAL_CURRENCY,
-  PAYPAL_RATE,
-  PAYPAL_ENV
-} from '../../constants';
+import { PAYPAL_CLIENT_ID, PAYPAL_CURRENCY, PAYPAL_RATE, PAYPAL_ENV } from '../../constants';
 
 /*
  * Update the input fields
@@ -57,6 +55,37 @@ export function isPayingWithPaypal(value) {
   };
 }
 
+export function createOrderExhibitor(ticketId, callback) {
+  return (dispatch) => {
+    const data = {
+      order_details: [
+        {
+          count: 1,
+          ticket_id: ticketId
+        }
+      ],
+      payment_type: 'offline'
+    };
+    payment
+      .post(data)
+      .then((response) => {
+        callback();
+        dispatch(getOrderDetail(response.data.data.id));
+      })
+      .catch(err => console.log(err));
+  };
+}
+
+export function getTickets() {
+  return (dispatch) => {
+    payment.get().then((response) => {
+      dispatch({
+        type: GET_TICKET_TYPES,
+        payload: response.data.data
+      });
+    });
+  };
+}
 /*
  * Initiate payment with PayPal
  * @param {order: order data}
@@ -70,27 +99,30 @@ export function payWithPaypal(order, callback = () => {}) {
       price: (order.price * order.count).toFixed(2).toString(),
       currency: PAYPAL_CURRENCY,
       description: 'Ticket for full 3-day event'
-    }).then(({ confirmation }) => {
-      const { response } = confirmation;
-      return getAccessToken()
-        .then((accessToken) => {
-          const data = {
-            order_id: order.order_id,
-            transaction_id: response.id,
-            payment_type: 'paypal'
-          };
-          return DevSummitAxios.post('/api/v1/payments/confirm', data, {
-            headers: { Authorization: accessToken }
-          });
-        }).then((result) => {
-          console.log('result', result);
-          dispatch(isPayingWithPaypal(false));
-          callback(result)
-        }).catch((error) => {
-          console.log('error', error)
-          dispatch(isPayingWithPaypal(false));
-        });
     })
+      .then(({ confirmation }) => {
+        const { response } = confirmation;
+        return getAccessToken()
+          .then((accessToken) => {
+            const data = {
+              order_id: order.order_id,
+              transaction_id: response.id,
+              payment_type: 'paypal'
+            };
+            return DevSummitAxios.post('/api/v1/payments/confirm', data, {
+              headers: { Authorization: accessToken }
+            });
+          })
+          .then((result) => {
+            console.log('result', result);
+            dispatch(isPayingWithPaypal(false));
+            callback(result);
+          })
+          .catch((error) => {
+            console.log('error', error);
+            dispatch(isPayingWithPaypal(false));
+          });
+      })
       .catch((error) => {
         console.log(error);
         dispatch(isPayingWithPaypal(false));
