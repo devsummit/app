ceda1ec6cfed5c486d6f7998b282217facdd483c
akbar <akbar_rachman@ymail.com>
2017-10-17 16:34:30 +0700

    change accordion style

--- app/containers/BoothList/Accordion.js
+++ app/containers/BoothList/Accordion.js
@@ -16,21 +16,23 @@ class AccordionView extends Component {
         duration={300}
         transition="backgroundColor"
         style={{
-          backgroundColor: isActive ? 'rgba(255,255,255,1)' : 'grey'
+          backgroundColor: isActive ? 'rgba(255,255,255,1)' : 'rgba(255,255,255,1)',
+          padding: 8
         }}
       >
-        <Card>
-          <View
-            style={{
-              flex: 1,
-              padding: 30,
-              alignItems: 'center',
-              flexDirection: 'column'
-            }}
-          >
-            <Text style={{ fontSize: 30, textAlign: 'center' }}>{section.title}</Text>
-          </View>
-        </Card>
+        <View
+          style={{
+            borderWidth: 5,
+            borderRadius: 5,
+            borderColor: '#BDBDBD',
+            flex: 1,
+            padding: 20,
+            alignItems: 'center',
+            flexDirection: 'column'
+          }}
+        >
+          <Text style={{ color: '#616161', fontSize: 30, textAlign: 'center' }}>{section.title}</Text>
+        </View>
       </Animatable.View>
     );
   };
@@ -41,13 +43,18 @@ class AccordionView extends Component {
       <Animatable.View
         duration={300}
         transition="backgroundColor"
-        style={{ backgroundColor: isActive ? 'rgba(255,255,255,1)' : 'rgba(245,252,255,1)' }}
+        style={{
+          backgroundColor: isActive ? '#E0E0E0' : 'rgba(245,252,255,1)',
+          borderRadius: 5,
+          borderColor: '#FFF',
+          marginHorizontal: 16
+        }}
       >
         <Animatable.View
           duration={300}
           easing="ease-out"
           animation={isActive ? 'zoomIn' : 'zoomInUp'}
-          style={{ margin: 16 }}
+          style={{ marginHorizontal: 16, padding: 8 }}
         >
           <Text style={{ fontSize: 24, fontWeight: 'bold' }}>Price: {section.price}</Text>
           {section.gifts &&
@@ -79,6 +86,7 @@ class AccordionView extends Component {
     return (
       <Accordion
         sections={SECTIONS}
+        underlayColor={'#FFFFFF'}
         renderHeader={this.renderHeader}
         renderContent={this.renderContent}
       />
