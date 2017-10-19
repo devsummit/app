8fe14cc854e3978f54a56015f195cdc30376f47c
Arnaz <arnazmursid@gmail.com>
2017-10-19 15:25:02 +0700

    add selector for get feedback

--- app/containers/Settings/selectors.js
+++ app/containers/Settings/selectors.js
@@ -4,9 +4,9 @@
 import { createSelector } from 'reselect';
 
 /**
- *  Select the main portion of the root reducer
+ *  Select the main portion of the root reducer from global reducer
  */
-const selectMainReducer = () => state => state.get('profile');
+const selectMainReducer = () => state => state.get('settings');
 
 /**
  *  Selects the username field data
@@ -45,4 +45,14 @@ export const getIsLogOut = () => createSelector(
 export const getIsDisabled = () => createSelector(
   selectMainReducer(),
   state => state.get('isDisabled')
+);
+
+export const getFeedback = () => createSelector(
+  selectMainReducer(),
+  state => state.get('feedBack')
+);
+
+export const getIsFeedbackPosted = () => createSelector(
+  selectMainReducer(),
+  state => state.get('isFeedbackPosted')
 );
