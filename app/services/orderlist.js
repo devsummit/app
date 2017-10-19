9bba1dc08b7eeace8872314697e9c4aec35935f2
Krisrp <kristoforusrp@gmail.com>
2017-10-20 00:27:12 +0700

    add service

--- app/services/orderlist.js
+++ app/services/orderlist.js
@@ -4,5 +4,6 @@ export default {
   get: () => Api.get('/api/v1/orders'),
   update: id => Api.patch(`/api/v1/payments/status/${id}`),
   countRedeem: () => Api.get('/api/v1/me'),
-  claimReward: () => Api.post('/api/v1/referals/reward', {})
+  claimReward: () => Api.post('/api/v1/referals/reward', {}),
+  postConfirmEmail: () => Api.post('/api/v1/confirm-email/resend', {})
 };
