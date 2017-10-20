8f315459691d493ca74cc9eb806fafc130390914
Yogi <yohgenius@gmail.com>
2017-10-20 14:53:37 +0700

    removing api/v1 prefix

--- app/services/payment.js
+++ app/services/payment.js
@@ -1,7 +1,7 @@
 import Api from './api';
 
 export default {
-  get: () => Api.get('/api/v1/tickets'),
-  post: payload => Api.post('/api/v1/orders', payload),
-  confirm: payload => Api.post('/api/v1/payments/confirm', payload)
+  get: () => Api.get('/tickets'),
+  post: payload => Api.post('/orders', payload),
+  confirm: payload => Api.post('/payments/confirm', payload)
 };
