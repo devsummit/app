2d8685e70ca023275c280f79faee188b50e95733
Yogi <yohgenius@gmail.com>
2017-10-20 14:54:28 +0700

    removing api/v1 prefix

--- app/services/ticketList.js
+++ app/services/ticketList.js
@@ -2,6 +2,6 @@ import Api from './api';
 
 export default {
   get: () => Api.get('/user/tickets'),
-  post: payloads => Api.post('/tickets/transfer')
+  post: payloads => Api.post('/tickets/transfer', payloads)
 }
 ;
