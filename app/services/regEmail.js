f7143fa822d440a61d093e479fa1b5953c145c74
Yogi <yohgenius@gmail.com>
2017-10-20 17:26:23 +0700

    adding api service

--- app/services/regEmail.js
+++ app/services/regEmail.js
@@ -1,7 +1,10 @@
 import Api from './api';
+import {
+  AUTH_BASE_URL
+} from '../constants';
 
 export default {
-  post: data => Api.post('/auth/register')
+  post: inputFields => Api.post(`${AUTH_BASE_URL}/auth/register`, inputFields)
   // getNext: nextUrl => 
 }
 ;
