01b0f6b5b13011f82c5a4639d9b2d7f28151f897
akbar <akbar_rachman@ymail.com>
2017-11-06 18:43:32 +0700

    remove log

--- app/containers/Feed/index.js
+++ app/containers/Feed/index.js
@@ -176,7 +176,6 @@ class Feed extends Component {
     this.props.setTokenHeader(this.props.currentPage);
 
     AsyncStorage.getItem('profile_data').then((profile) => {
-      console.log('fuckme here', profile);
       const data = JSON.parse(profile);
       const firstName = data.first_name;
       const lastName = data.last_name;
@@ -319,7 +318,6 @@ class Feed extends Component {
   };
 
   render() {
-      console.log('fuckme here bro', this.state.confirmed);
     return (
       <Container style={styles.container}>
         <HeaderPoint title={strings.feed.title} />
