ef40a31b691c0ccefd0f84a8e83b5ae810d2691c
akbar <akbar_rachman@ymail.com>
2017-11-06 19:53:24 +0700

    inisiate comment

---
+++ app/components/Comment/style.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    marginHorizontal: 8
+  }
+});
+
+export default styles;
---
+++ app/containers/CommentList/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+export default class CommentList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>DOING SOMETHING HERE</Text>
+      </View>
+    );
+  }
+}
