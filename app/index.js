7bdfbbc4fac50f5b95bc8f17426f59d0d2bd24a3
akbar <akbar_rachman@ymail.com>
2017-11-09 04:53:32 +0700

    add create post route

--- app/index.js
+++ app/index.js
@@ -43,6 +43,7 @@ import MyOrders from './components/MyOrders';
 import Drawer from './containers/Drawer';
 import SponsorInfo from './containers/SponsorInfo';
 import Comment from './containers/CommentList';
+import CreatePost from './containers/CreatePost';
 
 const RouterWithRedux = connect()(Router);
 const BackButtonImg = require('../assets/images/back.png');
@@ -138,6 +139,7 @@ export default class App extends Component {
                 <Scene key="myOrders" component={MyOrders} title="My Orders" />
                 <Scene key="sponsorInfo" component={SponsorInfo} title="Sponsor" />
                 <Scene key="comment" component={Comment} title="Comments" />
+                <Scene key="createPost" component={CreatePost} title="New post" />
               </Scene>
             </Scene>
           </RouterWithRedux>
