289df6917f409fa8a77b3e031b05f0caf4914b9c
Mochamad Gufron <mgufronefendi@gmail.com>
2017-11-11 04:14:40 +0700

    configure codepush

--- ios/app/AppDelegate.m
+++ ios/app/AppDelegate.m
@@ -23,7 +23,11 @@
 {
   NSURL *jsCodeLocation;
 
+#ifdef DEBUG
     jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
+#else
+    jsCodeLocation = [CodePush bundleURL];
+#endif
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"app"
