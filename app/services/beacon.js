8c4b328c3755dbbb4e10f0374282e727d35bfb42
Mochamad Gufron <mgufronefendi@gmail.com>
2017-11-20 20:13:20 +0700

    fix beacon

--- app/services/beacon.js
+++ app/services/beacon.js
@@ -1,9 +1,10 @@
-import { NativeEventEmitter, NativeModules, Alert } from 'react-native';
+import { NativeEventEmitter, AsyncStorage, NativeModules, Alert } from 'react-native';
 import { Actions } from 'react-native-router-flux';
+import moment from 'moment';
 import api from './api';
 import ticket from './ticket';
 import { store } from '../store';
-import { getProfileData } from "../helpers";
+import { getProfileData } from '../helpers';
 import { fetchTickets } from '../containers/OrderList/actions';
 import { userVisitedThisBooth, updateFabVisible } from '../containers/BoothInfo/actions';
 
@@ -26,21 +27,47 @@ const beacon = {
   },
   fetchBeacons: version => api.post('/api/v1/beacons/mapping/update', { version }),
   subscribe: callback => BeaconEventEmitter.addListener(events.beaconUpdate, callback),
+  checkinExhibitor: ({ major, minor }) => api.post('/api/v1/points/reward', { major, minor }),
   getBeacons: async () => {
-    const { data: { data } } = await beacon.fetchBeacons(1);
-    console.log('remote beacons', data);
-    return data;
+    try {
+      const beaconsData = await AsyncStorage.getItem('beacons');
+      // first initialization
+      if (!beaconsData) {
+        console.log('loading beacons from network');
+        const { data: { data } } = await beacon.fetchBeacons(1);
+        AsyncStorage.setItem('beacons', JSON.stringify({
+          beacons: data,
+          lastUpdatedAt: moment().format('YYYY-MM-DD HH:mm:ss')
+        }));
+        return data;
+      }
+      const { beacons, lastUpdatedAt } = JSON.parse(beaconsData);
+      if (moment(lastUpdatedAt).diff(moment()) >= 30 * 60) {
+        console.log('refreshing beacons from network');
+        const { data: { data } } = await beacon.fetchBeacons(1);
+        AsyncStorage.setItem('beacons', JSON.stringify({
+          beacons: data,
+          lastUpdatedAt: moment().format('YYYY-MM-DD HH:mm:ss')
+        }));
+        return data;
+      }
+      console.log('loading beacons from local storage', beacons);
+      return beacons;
+    } catch (e) {
+      console.log('error', e);
+      return [];
+    }
   },
   startRanging: () => {
     let visited;
-    Promise.all([beacon.getBeacons(), beacon.connect(), getProfileData()])
-      .then(([remoteBeacons, connectionResult, user]) => {
+    Promise.all([ beacon.getBeacons(), beacon.connect(), getProfileData() ])
+      .then(([ remoteBeacons, connectionResult, user ]) => {
         this.subscription = beacon.subscribe((beacons) => {
           if (remoteBeacons && remoteBeacons.length > 0) {
             const nearBeacon = beacons.find((item) => {
-              return Math.abs(item.accuracy) < 1;
+              return Math.abs(item.accuracy) < 1.5;
             });
-            console.log('nearBeacon', nearBeacon);
+            console.log('nearBeacon', nearBeacon, remoteBeacons);
             if (!nearBeacon || !remoteBeacons) {
               return;
             }
@@ -49,13 +76,14 @@ const beacon = {
                 && parseInt(item.minor, 0) === parseInt(nearBeacon.minor, 0));
             if (matchBeacon && (!visited ||
                 !(parseInt(visited.major, 0) === parseInt(matchBeacon.major, 0)
-                  && parseInt(visited.minor, 0) === parseInt(matchBeacon.minor, 0)))) {
+                  && parseInt(visited.minor, 0) === parseInt(matchBeacon.minor, 0)
+                ))) {
               visited = matchBeacon;
               beacon.onNearBeacon(matchBeacon, user);
             }
           }
         });
-      }).catch(e => console.log('error', e))
+      }).catch(e => console.log('error', e));
   },
   onNearBeacon: (matchBeacon, user) => {
     switch (matchBeacon.type) {
@@ -80,6 +108,7 @@ const beacon = {
     if (exhibitor && exhibitor.channel_id) {
       userVisitedThisBooth(exhibitor.channel_id).catch(e => console.log(e));
       store.dispatch(updateFabVisible(true));
+      beacon.checkinExhibitor(exhibitor);
       Actions.boothInfo({
         title: exhibitor.name,
         summary: exhibitor.summary,
@@ -102,18 +131,13 @@ const beacon = {
         Alert.alert('Oops', 'Looks like you have no ticket. You will get more insight if you have one or more tickets');
       } else if (tickets.count === 1) {
         Alert.alert('Glad to see you on this event!', 'We will check you in to let the people know you\'re here. Have fun on devsummit event.');
-        ticket.checkin(tickets[0].ticket_code).then(response => {
-          console.log('checkin response', response);
-        });
+        ticket.checkin(tickets[0].ticket_code).catch(e => console.log(e));
       } else {
         // must pick one ticket.
         // TODO: change this code so it should pick one of the ticket.
         Alert.alert('Glad to see you on this event!', 'We will check you in to let the people know you\'re here. Have fun on devsummit event.');
-        ticket.checkin(tickets[0].ticket_code).then(response => {
-          console.log('checkin response', response);
-        });
+        ticket.checkin(tickets[0].ticket_code).catch(e => console.log(e));
       }
-      // console.log('tickets', tickets);
     }));
   },
   onOther: (matchBeacon) => {
